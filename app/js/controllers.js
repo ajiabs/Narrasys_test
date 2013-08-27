22555f3954f5a584314e7a74c1a84604daca8fc4
Greg Tczap <livingegg@gmail.com>
2013-08-26 20:41:55 -0700

    episode controller should drive the ep template, enable application error route

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,27 @@
 
 // Declare the player.controllers module
 angular.module('player.controllers', [])
+
+// UI Error Controller (Shows an error through the UI)
+.controller('UIErrorController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
+	$scope.errorMsg = $rootScope.uiErrorMsg || 'The requested route does not exist. Please use /episode/<episodeId> format.';
+
+}])
 	
 // Episode Controller
-.controller('EpisodeController', ['$scope', function($scope) {
-	
+.controller('EpisodeController', ['$scope', '$rootScope', '$location', '$routeParams', '$http', function($scope, $rootScope, $location, $routeParams, $http) {
+
+	$http({method: 'GET', url: 'remote/data/episode-' + $routeParams.epId + '.json'})
+	.success(function(data, status, headers, config) {
+		$scope.episode = {
+			title: data.episode.title,
+			templateUrl: data.episode.template
+		};
+	})
+	.error(function(data, status, headers, config) {
+		$rootScope.uiErrorMsg = "Unable to load data for Episode: " + $routeParams.epId;
+		$location.path('/error');
+	});
+
 }]);
--- app/js/player.js
+++ app/js/player.js
@@ -13,17 +13,13 @@ angular.module('player', [
 // Configure routing
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-		.when('/', {
-			controller: function($scope) {
-				$scope.message = 'The requested route does not exist. Please use /episode/<episodeId> format.'
-			},
-			templateUrl: 'partials/error.html',
+		.when('/error', {
+			controller: 'UIErrorController',
+			templateUrl: 'partials/error.html'
 		})
-		.when('/episode/:episodeId', {
+		.when('/episode/:epId', {
 			controller: 'EpisodeController',
-			templateUrl: function(routeParams) {
-				return 'remote/templates/episode-' + routeParams.episodeId + '.html'
-			}
+			template: '<div ng-include="episode.templateUrl">Loading...</div>'
 		})
-		.otherwise({redirectTo: '/'});
+		.otherwise({redirectTo: '/error'});
 }]);
