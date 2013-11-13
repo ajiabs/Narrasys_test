870f809d686b687ef82c67a52db73ffe5dc845b2
Daniel Beck <daniel@danielbeck.net>
2013-11-13 15:41:34 -0500

    removing unused params from directive

--- app/scripts/directives/ittMagnetized.js
+++ app/scripts/directives/ittMagnetized.js
@@ -1,24 +1,24 @@
 'use strict';
 
-// ittMagnetized elements respond to videoMagnet events just like ittVideo.
+// ittMagnetized elements respond to videoMagnet events and reposition themselves to match
+// the active videoMagnet node.
 angular.module('com.inthetelling.player')
-.directive('ittMagnetized', function (queuePointScheduler, videojs, $timeout, config) {
+.directive('ittMagnetized', function () {
 	return {
 		restrict: 'A',
 		replace: true,
 		link: function(scope, iElement, iAttrs, controller) {
 	
 			console.log("ittMagnetized link");
-	
 
 			// listen for videoMagnet events and resize/reposition ourselves if we receive one
 			scope.$on('videoMagnet', function(evt, el) {
 			console.log("ittMagnetized triggered");
 			
 				// TODO: Animate?
-				//console.log("videoMagnet.on()!", evt, el);
 				if (el.parent().css("position") === "fixed") {
 					// if videoContainer is position:fixed, video should be too
+					// TODO: add equivalent padding to next pane element to prevent overlap
 					iElement.css("position","fixed");
 					iElement.offset(el.offset());
 					iElement.width(el.width());
