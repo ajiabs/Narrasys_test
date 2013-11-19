4661dd676ba8162ff056b1b28af995a329b25859
Daniel Beck <daniel@danielbeck.net>
2013-11-19 11:53:36 -0500

    add link function to ittItem to allow scenes to force a specific item template

--- app/scripts/directives/ittItem.js
+++ app/scripts/directives/ittItem.js
@@ -10,6 +10,20 @@ angular.module('com.inthetelling.player')
 		scope: {
 			item: '=ittItem'
 		},
+		link: function(scope, element, attrs) {
+			// check for forceTemplate attribute on the item ng-repeat.  If present, stash the real templateUrl in origTemplateUrl,
+			// and set a new templateUrl value for this scene only.
+			if (attrs.forceTemplate) {
+				console.log(scope.item);
+				scope.item.origTemplateUrl = scope.item.templateUrl
+				scope.item.templateUrl = "templates/transmedia-"+scope.item.type+"-"+attrs.forceTemplate+".html";
+			} else {
+				// no forceTemplate, so revert to the original (if there is one!)
+				if (scope.item.origTemplateUrl) {
+					scope.item.templateUrl = scope.item.origTemplateUrl;
+				}
+			}
+		},
 		controller: function($scope, modalMgr) {
 			// pass through method to modal.createItemDetailOverlay() for templates
 			$scope.launchDetailView = function() {
