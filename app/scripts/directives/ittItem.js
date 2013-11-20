63788c7894a1cfff531be99d0f3c5608102983fd
Daniel Beck <daniel@danielbeck.net>
2013-11-19 21:19:06 -0500

    Scene layouts done

--- app/scripts/directives/ittItem.js
+++ app/scripts/directives/ittItem.js
@@ -14,7 +14,6 @@ angular.module('com.inthetelling.player')
 			// check for forceTemplate attribute on the item ng-repeat.  If present, stash the real templateUrl in origTemplateUrl,
 			// and set a new templateUrl value for this scene only.
 			if (attrs.forceTemplate) {
-				console.log(scope.item);
 				scope.item.origTemplateUrl = scope.item.templateUrl;
 				scope.item.templateUrl = "templates/transmedia-"+scope.item.type+"-"+attrs.forceTemplate+".html";
 				// TODO: force layouts and styles too?  Ideally this won't be necessary, the layouts and styles will either be interchangeable or no-op, but will need to try it to be sure
--- app/scripts/directives/ittMagnetized.js
+++ app/scripts/directives/ittMagnetized.js
@@ -2,8 +2,9 @@
 
 // ittMagnetized elements respond to videoMagnet events and reposition themselves to match
 // the active videoMagnet node.
+
 angular.module('com.inthetelling.player')
-.directive('ittMagnetized', function () {
+.directive('ittMagnetized', function ($rootScope) {
 	return {
 		restrict: 'A',
 		replace: true,
@@ -11,14 +12,17 @@ angular.module('com.inthetelling.player')
 	
 			console.log("ittMagnetized link");
 
+			// TODO failed attempt to unbind previous magnet event in this scope
+			if (scope.magnetFunction) {scope.magnetFunction()};
+
 			// listen for videoMagnet events and resize/reposition ourselves if we receive one
-			scope.$on('videoMagnet', function(evt, el) {
-			console.log("ittMagnetized triggered");
-			
+			scope.magnetFunction = scope.$on('videoMagnet', function(evt, el) {
+				console.log("ittMagnetized triggered");
+
+
 				// TODO: Animate?
 				if (el.parent().css("position") === "fixed") {
 					// if videoContainer is position:fixed, video should be too
-					// TODO: add equivalent padding to next pane element to prevent overlap
 					iElement.css("position","fixed");
 					iElement.offset(el.offset());
 					iElement.width(el.width());
@@ -29,6 +33,19 @@ angular.module('com.inthetelling.player')
 					iElement.width(el.width());
 					iElement.height(el.height());
 				}
+				
+				// TODO HACK
+				// Need to refactor the videoMagnet, trigger it once per scene instead of
+				// once per magnetized element, and include this as part of it.... as it is this
+				// is 7 events when it could be one per scene
+				// Separating the following out into separate directives too
+				// would just multiply that problem, so for now I'm just going to handle it directly.
+				$('.currentScene .matchVideoHeight').height(el.height());
+				$('.currentScene .stretchToViewportBottom').each(function() {
+					$(this).css("min-height",($(window).height() - this.offsetTop - 60));
+				});
+
+
 			});
 		}
 	};
