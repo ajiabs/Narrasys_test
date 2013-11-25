186029824a915e053c40e26c98e3e967abc94ff3
Greg Tczap <livingegg@gmail.com>
2013-11-24 23:09:09 -0700

    wired up the results sorting dropdown

--- app/scripts/controllers/SearchPanelController.js
+++ app/scripts/controllers/SearchPanelController.js
@@ -5,7 +5,7 @@ angular.module('com.inthetelling.player')
 .controller('SearchPanelController', function ($scope, videojs) {
 
 	// map for the search panel checkboxes to bind to, which can also be passed to the type filter
-	$scope.selectedItemTypes = {
+	$scope.searchTypes = {
 		transcript: true,
 		link: true,
 		slide: true,
@@ -13,6 +13,32 @@ angular.module('com.inthetelling.player')
 		project: true,
 		discussion: true
 	};
+
+	// for the sort control to bind to
+	$scope.sortBy = "startTime";
+
+	// map type literals to pretty/printable version
+	$scope.prettyTypeMap = {
+		transcript: "Transcripts",
+		link: "Links",
+		slide: "Slides",
+		indepth: "In Depth",
+		project: "Projects",
+		discussion: "Discussions"
+	};
+
+	// create a dictionary of all items keyed by their type, for easy view iteration
+	// TODO: JSPerfTest this to see how expensive it is and move it to main controller if necessary
+	$scope.allItemsByType = {};
+	var i, j;
+	for (i=0; i < $scope.scenes.length; i++) {
+		for (j=0; j < $scope.scenes[i].items.length; j++) {
+			if (!$scope.allItemsByType[$scope.scenes[i].items[j].type]) {
+				$scope.allItemsByType[$scope.scenes[i].items[j].type] = [];
+			}
+			$scope.allItemsByType[$scope.scenes[i].items[j].type].push($scope.scenes[i].items[j]);
+		}
+	}
 
 	// convenience method for the template to change playhead position
 	$scope.gotoItem = function(item) {
