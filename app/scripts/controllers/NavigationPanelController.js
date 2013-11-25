7ae001690d7ec5f36301571e4764f66349209ebd
Greg Tczap <livingegg@gmail.com>
2013-11-24 21:43:38 -0700

    added linking to correct videjos time for search, and automatic closig of both panels

--- app/scripts/controllers/NavigationPanelController.js
+++ app/scripts/controllers/NavigationPanelController.js
@@ -4,10 +4,11 @@
 angular.module('com.inthetelling.player')
 .controller('NavigationPanelController', function ($scope, videojs) {
 
-	// convenience method for the template to change scenes
+	// convenience method for the template to change playhead position
 	$scope.gotoScene = function(scene) {
 		// set the video playhead to the scene's start time
 		videojs.player.currentTime(scene.startTime);
+		$scope.$close();
 	};
 
 });
--- app/scripts/controllers/SearchPanelController.js
+++ app/scripts/controllers/SearchPanelController.js
@@ -4,6 +4,7 @@
 angular.module('com.inthetelling.player')
 .controller('SearchPanelController', function ($scope, videojs) {
 
+	// map for the search panel checkboxes to bind to, which can also be passed to the type filter
 	$scope.selectedItemTypes = {
 		transcript: true,
 		link: true,
@@ -11,6 +12,13 @@ angular.module('com.inthetelling.player')
 		indepth: true,
 		project: true,
 		discussion: true
+	};
+
+	// convenience method for the template to change playhead position
+	$scope.gotoItem = function(item) {
+		// set the video playhead to the item's start time
+		videojs.player.currentTime(item.startTime);
+		$scope.$close();
 	};
 
 });
