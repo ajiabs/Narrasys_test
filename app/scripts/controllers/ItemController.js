a19d56286f30c7f2467514f42edf5a4eb316d4bc
Daniel Beck <daniel@danielbeck.net>
2013-12-02 13:03:51 -0500

    Moved showInlineDetail from controller to item directive, renamed to toggleDetailView, fixed window resize edge case

--- app/scripts/controllers/ItemController.js
+++ app/scripts/controllers/ItemController.js
@@ -2,17 +2,9 @@
 
 // Controller for ittItem
 angular.module('com.inthetelling.player')
-.controller('ItemController', function ($scope, $element, modalMgr, videojs) {
+.controller('ItemController', function ($scope, $element, videojs) {
 
-	// pass through method to modal.createItemDetailOverlay() for templates
-	$scope.launchDetailView = function() {
-		// If pane width > 400 show the iframe inline; otherwise pop a modal
-		if ($element.closest('.content').width() > 400) {
-			$scope.item.showInlineDetail = !$scope.item.showInlineDetail;
-		} else {
-			modalMgr.createItemDetailOverlay($scope);
-		}
-	};
+	
 	
 	// convenience method to set video time
 	$scope.gotoItem = function() {
