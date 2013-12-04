140edad8ae6c356cadbb5b9aba5a25b40b27fd22
Daniel Beck <daniel@danielbeck.net>
2013-12-04 15:36:16 -0500

    Sidebar/burst items in content panes now responsive to pane width
    it looks pretty awesome if I do say so myself

--- app/scripts/directives/ittScene.js
+++ app/scripts/directives/ittScene.js
@@ -2,23 +2,23 @@
 
 // Scene Directive
 angular.module('com.inthetelling.player')
-.directive('ittScene', function ($filter, $rootScope) {
+.directive('ittScene', function ($filter, $rootScope, $timeout) {
 	return {
 		restrict: 'A',
-		replace: true,
+		replace: false,
 		template: '<div ng-include="scene.templateUrl">Loading Scene...</div>',
 		scope: {
 			scene: '=ittScene'
 		},
-		link: function ($scope) {
+		link: function ($scope, element) {
 			// console.log("scene: ", $scope);
 			
 			// TODO: move this into modelFactory, just so we have all the precalc stuff in the same place?
 			// For each possible content pane, we're going to set variables for whether extra left or right margin will be needed
 			// there are three possible panes in a given scene (panes) which will be identified in the template by scene.sidebars.(paneNames[x])
 			$scope.scene.sidebars = {};
-			var panes = ["content", $scope.scene.mainPaneContents, $scope.scene.altPaneContents];
-			var paneNames = ["content","mainPane","altPane"];
+			var panes = [$scope.scene.mainPaneContents, $scope.scene.altPaneContents];
+			var paneNames = ["mainPane","altPane"];
 			for (var i=0; i<panes.length; i++) {
 				var contentlist = $filter('layout')($scope.scene.items,panes[i]);
 				for (var j=0; j < contentlist.length; j++) {
@@ -31,21 +31,40 @@ angular.module('com.inthetelling.player')
 					}
 				}
 			}
-		
-			// Set heights of elements within the scene based on viewport or video size.
+
+
+			// Make cosmetic adjustments within the scene based on viewport or video size.
 			// TODO: don't use jQuery here if possible?
-			// TODO: limit effects to this scene: I can't seem to get a handle on the element node from here...?
-			$scope.twiddleHeights = function() {
-				$('.matchVideoHeight:visible').height($('.videoContainer:visible').height());
-				$('.stretchToViewportBottom:visible').each(function() {
-					$(this).css("min-height",($(window).height() - this.offsetTop - 60));
-				});
-				// TODO: add stretchToSceneBottom as well (would be useful in explore template for example)
+			// TODO: (probably overoptimizing): have this function return immediately if the scene is not visible. (How do we check that without using jQuery?)
+			//       (can't use isActive, as sometimes inactive scenes are still visible)
+			var twiddleSceneLayout = function() {
+				$timeout(function() { // wait for any DOM updates first
+					console.log("twiddleSceneLayout");
+					element.find('.matchVideoHeight:visible').height(element.find('.videoContainer').height()); // TODO check if this works with multiple .matchVideoHeight elements in the scene
+					element.find('.stretchToViewportBottom:visible').each(function() {
+						$(this).css("min-height",($(window).height() - this.offsetTop - 60));
+					});
+					// TODO: add support for .stretchToSceneBottom as well (would be useful in explore template for example)
+	
+					// check each content pane and control whether it should allow or disallow sidebars (after DOM updates).  Breakpoint is currently 450px
+					// TODO (someday maybe): for some medium range of sizes, allow margin bursts but not full sidebars
+					var paneNames = ["mainPane","altPane"];
+					var paneClassNames = [".contentMain:visible",".contentAlt:visible"];
+					for (var i=0; i<paneNames.length; i++) {
+						var pane = element.find(paneClassNames[i]);
+						if (pane.width() && pane.width() < 450) {
+							$scope.scene.sidebars[paneNames[i]+"None"] = true;
+						} else {
+							$scope.scene.sidebars[paneNames[i]+"None"] = false;
+						}
+					}
+				},0);
 			}
 			
-			angular.element(window).bind('resize', $scope.twiddleHeights);
-			$rootScope.$on('toolbar.changedSceneTemplate', $scope.twiddleHeights);
-			$scope.$watch('scene.isActive', $scope.twiddleHeights);
+			angular.element(window).bind('resize', twiddleSceneLayout);
+			$rootScope.$on('toolbar.changedSceneTemplate', twiddleSceneLayout);
+			$scope.$watch('scene.isActive', twiddleSceneLayout);
+			twiddleSceneLayout(); // only needed for first scene draw
 		}
 	};
 });
