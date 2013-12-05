04c36dd99108cb89522fd064e9f4135a3aec1b14
Daniel Beck <daniel@danielbeck.net>
2013-12-05 14:35:50 -0500

    WTF ng-leave

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
 var config = {
-	localData: false, // player runs against local data if true, otherwise it runs against api data
+	localData: true, // player runs against local data if true, otherwise it runs against api data
 	apiDataBaseUrl: "http://api-dev-sub.inthetelling.com", // base url for the apis when running against api data
 	localDataBaseUrl: "/server-mock/data", // base url for the local data json files when running against local data
 	videoJSElementId: "vjs", // id to use for the main videojs element
