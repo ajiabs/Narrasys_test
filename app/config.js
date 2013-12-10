3cc21ee594c48514b0e59db6aad43d196acae1f2
Daniel Beck <daniel@danielbeck.net>
2013-12-10 16:43:24 -0500

    localData to false

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 var config = {
-	localData: true, // player runs against local data if true, otherwise it runs against api data
+	localData: false, // player runs against local data if true, otherwise it runs against api data
 	apiDataBaseUrl: 'http://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
 	apiAuthToken: 'Token token="c7624368e407355eb587500862322413"',
 	localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
