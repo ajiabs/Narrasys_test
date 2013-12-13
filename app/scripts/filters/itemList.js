ae5f24db75789f4bfc73989219550b5a844989ac
Daniel Beck <daniel@danielbeck.net>
2013-12-13 16:56:21 -0500

    itemList filter instead of ng-if

---
+++ app/scripts/filters/itemList.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+	Filter for content items, Used in contentpane.html
+	if showCurrent is true, returns only items for which isActive is true.
+	Otherwise just returns the entire list.
+
+*/
+angular.module('com.inthetelling.player')
+.filter('itemList', function () {
+	return function(items, showCurrent) {
+		if (showCurrent) {
+			var filteredItems = [], i;
+			for (i = 0; i < items.length; i++) {
+				if (items[i].isActive) {
+					filteredItems.push(items[i]);
+				}
+			}
+			console.log(filteredItems);
+			return filteredItems;
+		} else {
+			return items;
+		}
+
+	};
+});
