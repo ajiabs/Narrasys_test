f7750eaf30a874738ed0b8da3c98e4da813ec154
Daniel Beck <daniel@danielbeck.net>
2014-01-07 12:53:27 -0500

    Build and tag as v1.1.8

--- dist/config.js
+++ dist/config.js
@@ -1,10 +1,10 @@
 var config = {
-	localData: false, // player runs against local data if true, otherwise it runs against api data
+	localData: true, // player runs against local data if true, otherwise it runs against api data
 	apiDataBaseUrl: 'http://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
 	apiAuthToken: 'Token token="c7624368e407355eb587500862322413"',
 	localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
 	videoJSElementId: 'vjs', // id to use for the main videojs element
-	queuePointScanInterval: 10 // interval in ms when the queuePointScheduler service
+	cuePointScanInterval: 10 // interval in ms when the cuePointScheduler service
 							   // will perform a scan when dirty (see the service for more details).
 							   // lower numbers mean more responsiveness but potentially slower performance.
 							   // 10ms is the fastest that most modern browsers are able to do screen updates.
