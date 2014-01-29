9b5469463a832ad57b20e66599c2fa31236cdc74
Daniel Beck <daniel@danielbeck.net>
2014-01-29 14:04:08 -0500

    branding, bug fixes, redirect iDevice users to mp4 instead of youtube

--- app/scripts/services/videojs.js
+++ app/scripts/services/videojs.js
@@ -29,9 +29,9 @@ angular.module('com.inthetelling.player')
 					"nativeControlsForTouch": false // doesn't seem to do anything?
 				};
 				
-				// TODO Youtube plugin is buggy on iDevices and IE9.  If we cant fix and can afford the bandwidth, divert those users to S3
-				//if (videodata.youtube && !($rootScope.isIPad || $rootScope.isIPhone)) { 
-				if (videodata.youtube) {
+				// TODO Youtube plugin is buggy on iDevices and IE9.  If we cant fix and can afford the bandwidth, divert those users to S3.
+				// SEE ALSO ittVideo.js which needs to perform the same test!
+				if (videodata.youtube && !($rootScope.isIPad || $rootScope.isIPhone)) { 
 					vjsconfig.techOrder = ["youtube"];
 				} else {
 					vjsconfig.techOrder = ["html5", "flash"];
