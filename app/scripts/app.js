504e959f0b4b5d0dac8e14db1b31642544e2b138
Daniel Beck <daniel@danielbeck.net>
2014-01-29 18:34:33 -0500

    http headers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,17 +33,21 @@ angular.module('com.inthetelling.player', ['ngRoute', 'ngAnimate'])
 		/^https?:\/\/danielbeck.net/,
 		/^https?:\/\/platformuniv-p.edgesuite.net/
 	]);
-});
+})
 
-/*
 // Configure http headers
 .config(function($httpProvider) {
+	/*
 	$httpProvider.defaults.headers.get = {
 		'Authorization': 'Token token="c7624368e407355eb587500862322413"',
 		'Content-Type': 'application/json'
 	};
+	*/
+	$httpProvider.defaults.useXDomain = true;
+	$httpProvider.defaults.withCredentials = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	console.log($httpProvider);
 });
-*/
 
 /*
 .run(['timelineSvc', '$window', function(timelineSvc, $window) {
