04363f3c0665d6c9f27e38c4cbb9026f66481db7
Daniel Beck <daniel@danielbeck.net>
2014-02-04 22:43:48 -0500

    Authentication changes for Bill
    API base URL now depends on player’s current subdomain

--- app/config.js
+++ app/config.js
@@ -1,7 +1,12 @@
 var config = {
 	localData: false, // player runs against local data if true, otherwise it runs against api data
-	apiDataBaseUrl: '//story-api.inthetelling.com', // base url for the apis when running against api data
+
+	// NOTE WARN if you change the below field, test scripts/services/config.js which modifies this
+	apiDataBaseUrl: '//story-api.inthetelling.com', // default base url for the apis when running against api data
+
 	localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
+
 	videoJSElementId: 'vjs', // id to use for the main videojs element
+
 	cuePointScanInterval: 10 // interval in ms when the cuePointScheduler service will perform a scan when dirty (see the service for more details).
 };
