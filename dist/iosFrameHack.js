5239cea111067d36cb2621b7fbe13b646cbb05d9
Daniel Beck <daniel@danielbeck.net>
2014-02-14 16:42:44 -0500

    v1.3.11 frames can change size

--- dist/iosFrameHack.js
+++ dist/iosFrameHack.js
@@ -1,12 +1,18 @@
+/* 
+ Hacky way to work around the 3rd-party iframe cookie block in iOS and (apparently) chrome
+ The site that frames us must include this script in their page head (and must be using jquery!)
+ 
+ Will be obsolete when we switch to using header tokens instead of cookies 
+*/
+
+
 jQuery(document).ready(function() {
-	if (navigator.platform.indexOf('iPhone') > -1 || navigator.platform.indexOf('iPod') > -1) {
 		jQuery('iframe').each(function() {
 			var src = jQuery(this).attr("src");
-			if (src.indexOf('inthetelling.com/#/episode')>-1) {
+			if (src.indexOf('inthetelling.com/')>-1 && src.indexOf('episode')>-1) {
 				var delimiter = (src.indexOf('?') > -1) ? "&" : "?" ;
 				src = src + delimiter + "return_to=" + document.location.href;
 				jQuery(this).attr("src", src);
 			}
 		});
-	}
 });
