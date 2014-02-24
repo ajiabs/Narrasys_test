714b1b6fcd52fb2cafe68ff7751de870632b7d89
Daniel Beck <daniel@danielbeck.net>
2014-02-24 17:18:20 -0500

    Making apiDataBaseUrl optional

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -5,12 +5,9 @@ angular.module('com.inthetelling.player')
 	.factory('config', function () {
 		var config = window.config;
 
-		// If we're running on an inthetelling.com domain, need to change the API key to match the current subdomain.  This is for authentication, ask bill
-		// This will match *all* of them, for example http://www.foo.bar.inthetelling.com would return "www.foo.bar" as the subdomain
-		var currentSubdomain = window.location.href.match(/\/\/(.*)\.inthetelling.com/);
-		if (currentSubdomain) { // If that didn't match anything we'll just leave the existing apiData url as is.
-			// swap the user's subdomain into the apiDataBaseUrl's subdomain (which I'm trying hard not to hardcode here)
-			config.apiDataBaseUrl = config.apiDataBaseUrl.replace(/\/\/.*\.inthetelling.com/,"//"+currentSubdomain[1]+".inthetelling.com");
+		// Find out the API data url, if not specified:
+		if (!config.apiDataBaseUrl) {
+			config.apiDataBaseUrl = window.location.host;
 		}
 		return config;
 	});
