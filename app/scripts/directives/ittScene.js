e9ead63ff11aa90bff2229000184cf8fd4396f69
Daniel Beck <daniel@danielbeck.net>
2014-02-26 12:29:12 -0500

    Added extraneous semicolons to keep jshint happy

--- app/scripts/directives/ittScene.js
+++ app/scripts/directives/ittScene.js
@@ -82,7 +82,7 @@ angular.module('com.inthetelling.player')
 				
 				// iframes on ipads plus window.resize watchers == crash.
 				if ($rootScope.isFramed && ($rootScope.isIPad || $rootScope.isIPhone)) {
-					scope.$watch(function() {return scope.IOSFramewidth},twiddleSceneLayout);
+					scope.$watch(function() {return scope.IOSFramewidth;},twiddleSceneLayout);
 				} else {
 					angular.element($window).bind('resize', twiddleSceneLayout);
 				}
