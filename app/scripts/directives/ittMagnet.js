7c7aaeb358e91381047279ab725c03246078874a
Daniel Beck <daniel@danielbeck.net>
2014-03-18 22:28:57 -0400

    Removing unnecessary comment

--- app/scripts/directives/ittMagnet.js
+++ app/scripts/directives/ittMagnet.js
@@ -1,10 +1,6 @@
 'use strict';
 
-// This never worked the way it was supposed to, involved a bizarre number of events being passed around for a fairly simple action,
-// and was killing performance.
-
-// I'm putting it to bed.  Instead of having to put watchers and broadcasters on every single magnet all the time,
-// we'll keep a singleton active magnet in (for now) rootscope and let the magnetized elements use that as their target.
+// Not currently in use
 
 angular.module('com.inthetelling.player')
 	.directive('ittMagnet', function ($window, $timeout, $rootScope) {
