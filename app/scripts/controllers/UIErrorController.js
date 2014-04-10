7439687f5e56ad673227ca25794558e9128aba9d
Daniel Beck <daniel@danielbeck.net>
2014-04-10 09:16:37 -0400

    Added delay before error message display

--- app/scripts/controllers/UIErrorController.js
+++ app/scripts/controllers/UIErrorController.js
@@ -2,14 +2,17 @@
 
 // UI Error Controller
 angular.module('com.inthetelling.player')
-	.controller('UIErrorController', function ($scope, $rootScope,$location) {
-		$rootScope.$on("error", function(event, errorData) {
-			$scope.error = errorData;
+	.controller('UIErrorController', function ($scope, $rootScope, $timeout, $location) {
+		$rootScope.$on("error", function (event, errorData) {
+			// delay to prevent displaying transient errors on page refresh
+			$timeout(function () {
+				$scope.error = errorData;
+			}, 2000);
 		});
 
-// Would prefer not to have app.js redirect to /error in the first place, but as long as we are for missing routes:
+		// Would prefer not to have app.js redirect to /error in the first place, but as long as we are for missing routes:
 		if ($location.url() === "/error") {
-			$rootScope.$emit("error",{
+			$rootScope.$emit("error", {
 				"message": "Requested rote doesn't exist."
 			});
 		}
