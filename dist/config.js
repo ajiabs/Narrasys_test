1ef02efc86c20d2bd83c82cf60f6e5fa8a56f469
Daniel Beck <daniel@danielbeck.net>
2014-06-30 18:17:41 -0400

    build

--- dist/config.js
+++ dist/config.js
@@ -1,11 +1,13 @@
 var config = {
-	//localData: false, // player runs against local data if true, otherwise it runs against api data
 
 	// Fill this in only if you need to load data from something other than the current server
-	apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
+	apiDataBaseUrl: 'https://exec-forum.inthetelling.com', // base url for the apis when running against api data
 
-	localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
+	disableYoutube: false,
+	// localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
 
-	//videoJSElementId: 'vjs', // id to use for the main videojs element
 
 };
+
+
+//https://exec-forum.inthetelling.com/#/episode/53a9fcedbf31cdf152000004
