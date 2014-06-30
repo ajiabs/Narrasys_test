87c9e016862b5cade25a132a6b9c947c3a059ee0
Daniel Beck <daniel@danielbeck.net>
2014-06-30 18:19:29 -0400

    v4 alpha 9

--- app/config.js
+++ app/config.js
@@ -1,13 +1,7 @@
 var config = {
-
 	// Fill this in only if you need to load data from something other than the current server
-	apiDataBaseUrl: 'https://exec-forum.inthetelling.com', // base url for the apis when running against api data
+	apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
 
-	disableYoutube: false,
-	// localDataBaseUrl: '/server-mock/data', // base url for the local data json files when running against local data
-
+	disableYoutube: false
 
 };
-
-
-//https://exec-forum.inthetelling.com/#/episode/53a9fcedbf31cdf152000004
