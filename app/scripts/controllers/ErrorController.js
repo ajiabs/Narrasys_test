bc242f359e699a743cbe647d03d03ffc9aec0d4f
Daniel Beck <daniel@danielbeck.net>
2014-07-12 15:53:56 -0400

    separated errorSvc

---
+++ app/scripts/controllers/ErrorController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('com.inthetelling.player')
+.controller('ErrorController', function($scope, $rootScope, errorSvc, appState) {
+	$scope.errors = errorSvc.errors;
+	$scope.user = appState.user;
+
+	$scope.dismiss = function(error) {
+		console.log(error, $scope.errors);
+		var ret = [];
+		for (var i = 0; i < $scope.errors.length; i++) {
+			if ($scope.errors[i] !== error) {
+				ret.push($scope.errors[i]);
+			}
+		}
+		$scope.errors = ret;
+	};
+
+});
