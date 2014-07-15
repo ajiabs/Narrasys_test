34a50a015d1d60010e8f43a6993ed348e1b69526
Daniel Beck <daniel@danielbeck.net>
2014-07-14 23:34:34 -0400

    so much for round numbers

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,7 @@ var config = {
 	// Fill this in only if you need to load data from something other than the current server
 	// apiDataBaseUrl: 'https://e-literate-tv.inthetelling.com', // base url for the apis when running against api data
 	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
-	apiDataBaseUrl: 'https://gwsb.inthetelling.com', // base url for the apis when running against api data
+	// apiDataBaseUrl: 'https://gwsb.inthetelling.com', // base url for the apis when running against api data
 	disableYoutube: false
 
 };
