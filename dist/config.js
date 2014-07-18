30e59bc1979e8a589bcfd610aa8e825e539f85b7
Daniel Beck <daniel@danielbeck.net>
2014-07-18 15:52:11 -0400

    v4.0.10

--- dist/config.js
+++ dist/config.js
@@ -3,6 +3,9 @@ var config = {
 	// apiDataBaseUrl: 'https://e-literate-tv.inthetelling.com', // base url for the apis when running against api data
 	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com', // base url for the apis when running against api data
 	// apiDataBaseUrl: 'https://gwsb.inthetelling.com', // base url for the apis when running against api data
-	disableYoutube: false
+	// apiDataBaseUrl: 'https://ewb-usa.inthetelling.com', // base url for the apis when running against api data
+	localStorageKey: "storyKey",
+	disableYoutube: false,
+	disableAnalytics: false
 
 };
