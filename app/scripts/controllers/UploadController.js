f236266433103f31d1942dcb1bf64e1a9f117537
Bill Snyder <bill@sizzle.local>
2014-07-31 12:08:56 -0600

    Started adding AWS S3 Support

--- app/scripts/controllers/UploadController.js
+++ app/scripts/controllers/UploadController.js
@@ -1,21 +1,14 @@
 'use strict';
 
 angular.module('com.inthetelling.story')
-	.controller('UploadController', function ($scope, config, awsSvc, appState) {
-
-	        $scope.loading = true;
-		awsSvc.getUploadSession().then(function (data) {
-			$scope.credentials = data;
-		        $scope.loading = false;
-		});
-
-		$scope.authenticate = function (episodeID) {
-			console.warn("clearing localStorage for reauthentication");
-			if (localStorage) {
-				localStorage.removeItem(config.localStorageKey);
-			}
-			appState.user = {};
-			window.location = (config.apiDataBaseUrl + "/pages/launch_oauth2?episode=" + episodeID);
-
-		};
+    .controller('UploadController', function ($scope, config, awsSvc) {
+    //.controller('UploadController', function ($scope, config, awsSvc, appState) {
+	
+	$scope.loading = true;
+	awsSvc.getBucketListing().then(function (data) {
+	    $scope.bucket_objects = data.Contents;
+	    $scope.loading = false;
 	});
+	
+	
+    });
--- app/scripts/services/awsSvc.js
+++ app/scripts/services/awsSvc.js
@@ -1,38 +1,79 @@
 'use strict';
 
 angular.module('com.inthetelling.story')
-	.factory('awsSvc', function (config, $routeParams, $http, $q, appState) {
-		console.log('awsSvc factory, user is guest: '+appState.user.guest);
-		var svc = {};
-		var awsCache = {
-			uploadSession: {}
-		};
+    .factory('awsSvc', function (config, $routeParams, $http, $q, appState) {
+	console.log('awsSvc, user: ', appState.user);
+	var svc = {};
+	var awsCache = {
+            s3: {}
+	};
+	
+	svc.getUploadSession = function () {
+	    if (awsCache.hasOwnProperty('sessionDeferred')) {
+		return awsCache.sessionDeferred.promise;
+	    } else {
+		awsCache.sessionDeferred = $q.defer();
+	    }
+	    $http.get(config.apiDataBaseUrl + "/v1/get_s3_upload_session")
+		.success(function (data) {
+		    if (data.access_key_id) {
+			AWS.config.update({accessKeyId: data.access_key_id, secretAccessKey: data.secret_access_key, sessionToken: data.session_token, region: config.awsRegion});
+			var params = {
+			    params: {
+				Bucket: data.bucket,
+				Prefix: data.key_base
+			    }
+			};
+                        awsCache.s3 = new AWS.S3(params);
+			awsCache.sessionDeferred.resolve(data);
+		    } else {
+			awsCache.sessionDeferred.reject();
+		    }
+		})
+		.error(function () {
+		    awsCache.sessionDeferred.reject();
+		});
+	    return awsCache.sessionDeferred.promise;
+	};
 
-		svc.getUploadSession = function () {
-			var defer = $q.defer();
-			$http.get(config.apiDataBaseUrl + "/v1/get_s3_upload_session")
-				.success(function (data) {
-					if (data.access_key_id) {
-					        awsCache.uploadSession = data;
-						defer.resolve(data);
-					} else {
-						// Guest access is not allowed
-						if (data.login_url) {
-							if (data.login_via_top_window_only) {
-								window.top.location.href = data.login_url;
-							} else {
-								window.location.href = data.login_url;
-							}
-						} else {
-							defer.reject();
-						}
-					}
-				})
-				.error(function () {
-					defer.reject();
-				});
-			return defer.promise;
-		};
-
-	        
-	});
+	svc.getBucketListing = function() {
+	    var defer = $q.defer();
+	    svc.getUploadSession().then(function listObjects() {
+		awsCache.s3.listObjects(function(err, data) {
+		    if (err) {
+			console.log(err, err.stack); // an error occurred
+			defer.reject();
+		    } else {
+			console.log('awsSvc, got bucket listing!', data);
+			defer.resolve(data);           // successful response
+		    }
+		});
+	    });
+            
+	    return defer.promise;
+	    
+        };
+	
+	svc.createMultipartUpload = function() {
+	    var defer = $q.defer();
+	    svc.getUploadSession().then(function createMultipartUpload() {
+		awsCache.s3.listObjects(function(err, data) {
+		    if (err) {
+			console.log(err, err.stack); // an error occurred
+			defer.reject();
+		    } else {
+			console.log('awsSvc, got bucket listing!', data);
+			defer.resolve(data);           // successful response
+		    }
+		});
+	    });
+	    return defer.promise;
+	};
+	
+	svc.awsCache = function() {
+	    return awsCache;
+	};
+	
+	return svc;
+        
+    });
