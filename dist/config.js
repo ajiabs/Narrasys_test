ee2346b256962b0c282a5f86810f87890031a1de
Daniel Beck <daniel@danielbeck.net>
2014-08-04 21:40:52 -0400

    v4.0.24

--- dist/config.js
+++ dist/config.js
@@ -2,11 +2,11 @@ var config = {
 	// Fill this in only if you need to load data from something other than the current server
 	// apiDataBaseUrl: 'https://e-literate-tv.inthetelling.com',
 	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com',
-	// apiDataBaseUrl: 'https://gwsb.inthetelling.com',
+	apiDataBaseUrl: 'https://gwsb.inthetelling.com',
 	// apiDataBaseUrl: 'https://ewb-usa.inthetelling.com',
 	// apiDataBaseUrl: 'https://story.inthetelling.com',
 	// apiDataBaseUrl: 'https://purdue.inthetelling.com',
-	apiDataBaseUrl: 'https://usc-scholars.inthetelling.com',
+	// apiDataBaseUrl: 'https://usc-scholars.inthetelling.com',
 	localStorageKey: "storyKey",
 	disableYoutube: false,
 	disableAnalytics: false
