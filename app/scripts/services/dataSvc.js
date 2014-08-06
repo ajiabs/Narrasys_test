7aa0457165e8736be2e7ecabbca9ec1ef9b797b8
Daniel Beck <daniel@danielbeck.net>
2014-08-06 14:15:13 -0400

    improved search on small screens; banner logo fixes

--- app/scripts/services/dataSvc.js
+++ app/scripts/services/dataSvc.js
@@ -399,7 +399,7 @@ angular.module('com.inthetelling.story')
 				"master_asset_id": "masterasset",
 				"title": "Test Episode",
 				"status": "Published",
-				"templateUrl": "templates/episode/gw.html",
+				"templateUrl": "templates/episode/story.html",
 				"styles": [
 					"", "", ""
 				]
