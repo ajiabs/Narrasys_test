28d3d0c5c559e7ced8745655ab0b8f70d922e414
Daniel Beck <daniel@danielbeck.net>
2014-09-16 11:04:31 -0400

    v4.0.45

--- dist/config.js
+++ dist/config.js
@@ -10,6 +10,6 @@ var config = {
 	localStorageKey: "storyKey",
 	disableYoutube: false,
 	disableAnalytics: false,
-	debugInBrowser: true // Set this to false to make karma less noisy.  This setting will have no effect in production builds, which drop all console logs anyway. 
+	debugInBrowser: false // Set this to false to make karma less noisy.  This setting will have no effect in production builds, which drop all console logs anyway. 
 
 };
