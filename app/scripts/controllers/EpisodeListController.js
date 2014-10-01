f3bf1e78e592938c27f3d04ce60b8c97bd4ccdca
Daniel Beck <daniel@danielbeck.net>
2014-10-01 12:42:00 -0400

    Merge branch 'dev'

--- app/scripts/controllers/EpisodeListController.js
+++ app/scripts/controllers/EpisodeListController.js
@@ -3,24 +3,9 @@
 angular.module('com.inthetelling.story')
 	.controller('EpisodeListController', function ($scope, config, dataSvc) {
 
-		// dataSvc.getEpisodeList().then(function (data) {
-		// 	$scope.episodes = data;
-		// });
 		$scope.loading = true;
 		dataSvc.getAllContainers().then(function (data) {
 			$scope.containers = data;
 			$scope.loading = false;
 		});
-
-		// $scope.user = appState.user;
-
-		// $scope.authenticate = function (episodeID) {
-		// 	console.warn("clearing localStorage for reauthentication");
-		// 	if (localStorage) {
-		// 		localStorage.removeItem(config.localStorageKey);
-		// 	}
-		// 	appState.user = {};
-		// 	window.location = (config.apiDataBaseUrl + "/pages/launch_oauth2?episode=" + episodeID);
-
-		// };
 	});
