2599c900eaad6199183923699e998470f4f6b518
Daniel Beck <daniel@danielbeck.net>
2014-11-19 15:05:17 -0500

    some role-based rules on /#/episodes route

--- app/scripts/controllers/EpisodeListController.js
+++ app/scripts/controllers/EpisodeListController.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('com.inthetelling.story')
-	.controller('EpisodeListController', function ($scope, config, dataSvc) {
+	.controller('EpisodeListController', function ($scope, config, dataSvc, authSvc) {
 
 		$scope.loading = true;
 		dataSvc.getAllContainers().then(function (data) {
 
 			$scope.containers = data;
 			$scope.loading = false;
+
+			$scope.userHasRole = function (role) {
+				return authSvc.userHasRole(role);
+			};
+
+			$scope.loginTo = function (epId) {
+				console.log("EPID: ", epId);
+				localStorage.removeItem(config.localStorageKey);
+				window.open(config.apiDataBaseUrl + "/oauth2?episode=" + epId);
+
+			};
+
 		});
 	});
