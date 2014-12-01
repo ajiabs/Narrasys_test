294e3df73da2a8cd4f79d3bf59e05eaebc76400e
Daniel Beck <daniel@danielbeck.net>
2014-12-01 13:16:38 -0500

    config cleanup

--- app/config.js
+++ app/config.js
@@ -1,17 +1,22 @@
 var config = {
 	// Fill this in only if you need to load data from something other than the current server
+
+	// apiDataBaseUrl: 'https://demo.inthetelling.com',
+	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com',
+
 	// apiDataBaseUrl: 'https://e-literate-tv.inthetelling.com',
-	// apiDataBaseUrl: 'https://middlebury.inthetelling.com',
 	// apiDataBaseUrl: 'https://gwsb.inthetelling.com',
+	// apiDataBaseUrl: 'https://middlebury.inthetelling.com',
 	// apiDataBaseUrl: 'https://ewb-usa.inthetelling.com',
 	// apiDataBaseUrl: 'https://story.inthetelling.com',
 	// apiDataBaseUrl: 'https://purdue.inthetelling.com',
 	// apiDataBaseUrl: 'https://usc-scholars.inthetelling.com',
-	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com',
-	// apiDataBaseUrl: 'https://demo.inthetelling.com',
+	// apiDataBaseUrl: 'https://sustainablebizeducation.inthetelling.com',
+
 	localStorageKey: "storyKey",
+	awsRegion: 'us-east-1',
 	disableYoutube: false,
-	disableAnalytics: true,
-	debugInBrowser: true, // Set this to false to make karma less noisy.  This setting will have no effect in production builds, which drop all console logs anyway. 
-	awsRegion: 'us-east-1'
+	disableAnalytics: false,
+	debugInBrowser: false // Set this to false to make karma less noisy.  This setting will have no effect in production builds, which drop all console logs anyway. 
+
 };
