01b8cc61153676886be85e9c40f0e6cce02e6ea3
Daniel Beck <daniel@danielbeck.net>
2014-12-01 13:28:55 -0500

    v4.2.15

--- app/scripts/controllers/EpisodeController.js
+++ app/scripts/controllers/EpisodeController.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('com.inthetelling.story')
-	.controller('EpisodeController', function ($scope, $routeParams, modelSvc, appState) {
+	.controller('EpisodeController', function () {
 
 	});
