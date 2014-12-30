006c5712a2c88912fb290af3f2585949b0f5e5f1
Daniel Beck <daniel@danielbeck.net>
2014-12-30 11:54:58 -0500

    last commit only contained half of the fix, oops

--- app/scripts/services/timelineSvc.js
+++ app/scripts/services/timelineSvc.js
@@ -185,7 +185,7 @@ angular.module('com.inthetelling.story')
 			appState.time = t;
 			videoScope.seek(t);
 			svc.updateEventStates();
-			stepEvent();
+			stepEvent(true);
 
 			// capture analytics data:
 			if (method) {
