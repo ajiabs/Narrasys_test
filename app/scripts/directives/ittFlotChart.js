3195b1873c116ed59d31f1ce6149ba73fc795ec6
Matt Self <self.matt@gmail.com>
2015-01-05 15:04:09 -0700

    make the pie chart accessible for screen readers.

--- app/scripts/directives/ittFlotChart.js
+++ app/scripts/directives/ittFlotChart.js
@@ -10,19 +10,22 @@
 				width: '@',
 				title: '=title', //title is separate from options, but we may not want this as i'm not sure ALL charts have a title (or just one)
 				options: '@',
-				data: '@'
+				data: '@',
+				chartLabel: '@'
 			},
-			template: '<div id="chartContainer"></div>',
+template: ' <div id="chartContainer" aria-label="{{chartLabel}}"><table><caption>Others answered this question as follows:</caption> <tbody><tr ng-repeat="x in data.series"> <th scope="col">Choice</th> <th scope="col">Percent</th> </tr> <tr> <th scope="row">Jackie {{x}}</th> <td>5</td> </tr> </tbody></table></div>', 
 			link: function (scope, element, attrs) {
 				var chartContainer;
 				var width = 500;
 				var height = 500;
 
 				var draw = _.after(2, function (el, d, o) {
+					scope.chartLabel = createLabel(d);
 					o.series.pie.label.formatter = function (label, series) {
 						return '<div style="font-size:8pt;text-align:center;padding:2px;color:black;">' + label + '<br/>' + Math.round(series.data[0][1]) + '%</div>';
 					};
 					$.plot(el, d, o);
+
 					el.show();
 				});
 
@@ -44,7 +47,13 @@
 					scope['options'] = JSON.parse(value);
 					draw(chartContainer, scope.data, scope.options);
 				});
-
+				var createLabel = function(data) {
+					var labelText = "";	
+					for (var i=0; i < data.length; i++) {
+						labelText += Math.round(data[i].data) + "% of users choose \'" + data[i].label + "\'. ";
+					}
+					return labelText;
+				};
 			}
 		};
 	});
