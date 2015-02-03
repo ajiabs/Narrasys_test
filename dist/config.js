aa6d38085b6c18fb93c1eb2e8094629bff58f1f7
Daniel Beck <daniel@danielbeck.net>
2015-02-03 10:52:19 -0500

    v4.3.10

--- dist/config.js
+++ dist/config.js
@@ -3,7 +3,6 @@ var config = {
 
 	// apiDataBaseUrl: 'https://demo.inthetelling.com',
 	// apiDataBaseUrl: 'https://api-dev-sub.inthetelling.com',
-
 	// apiDataBaseUrl: 'https://e-literate-tv.inthetelling.com',
 	// apiDataBaseUrl: 'https://gwsb.inthetelling.com',
 	// apiDataBaseUrl: 'https://ewb-usa.inthetelling.com',
@@ -12,8 +11,8 @@ var config = {
 	// apiDataBaseUrl: 'https://usc-scholars.inthetelling.com',
 	// apiDataBaseUrl: 'https://sustainablebizeducation.inthetelling.com',
 	// apiDataBaseUrl: 'https://columbiabusinessschool.inthetelling.com',
-
 	//apiDataBaseUrl: 'https://middlebury.inthetelling.com',
+
 	localStorageKey: "storyKey",
 	disableYoutube: false,
 	disableAnalytics: false,
