4931bb2dceca70fc2acbadd2ce93f7b721d57cdf
Daniel Beck <daniel@danielbeck.net>
2015-02-12 08:51:09 -0500

    test

--- app/config.js
+++ app/config.js
@@ -10,6 +10,7 @@ var config = {
 	// apiDataBaseUrl: 'https://purdue.inthetelling.com',
 	// apiDataBaseUrl: 'https://usc-scholars.inthetelling.com',
 	// apiDataBaseUrl: 'https://sustainablebizeducation.inthetelling.com',
+
 	// apiDataBaseUrl: 'https://columbiabusinessschool.inthetelling.com',
 	// apiDataBaseUrl: 'https://middlebury.inthetelling.com',
 
