07f0fb34f4ffed8426ae2fe60bbc7315ff5e435c
Daniel Beck <daniel@danielbeck.net>
2015-02-12 11:15:25 -0500

    auth route now supports narrative param

--- app/scripts/directives/ittLogin.js
+++ app/scripts/directives/ittLogin.js
@@ -33,13 +33,17 @@ angular.module('com.inthetelling.story')
 						$location.search('episode', null);
 						$location.search('nonce', null);
 						$location.path('/episode/' + epId);
+					} else if ($routeParams.narrative) {
+						var narrId = $routeParams.narrative;
+						$location.search('narrative', null);
+						$location.search('nonce', null);
+						$location.path('/story/' + narrId);
 					} else {
 						// Don't redirect by default, as it breaks LTI login.  
 						// TODO investigate why that started breaking LTI login (it hits /auth with no episode param at least once...)
 						if (scope.userHasRole('admin') && Object.keys($routeParams).length === 0) {
 							$location.path('/');
 						}
-
 					}
 				}, function () {
 					console.log("Login failed...");
