fc273407cc518dc65d83665f70c1d39ea393bcdf
Matt Self <self.matt@gmail.com>
2015-02-28 14:17:08 -0700

    whitespace

--- app/scripts/directives/ittEpisodeEditor.js
+++ app/scripts/directives/ittEpisodeEditor.js
@@ -284,7 +284,7 @@ angular.module('com.inthetelling.story')
 						}
 					} else {
 						hasMasterAsset = false;
-					}					
+					}
 
 					getYoutubeDuration(url)
 						.then(function (duration) {
