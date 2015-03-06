4c2a0530d1b4d1dd2a9db3479b1ee7eca91eb53e
Matt Self <self.matt@gmail.com>
2015-03-06 11:13:35 -0700

    return additional values from js api and hide the youtube api contract to shelter us from some changes or at least prevent them from spreading

--- app/scripts/services/youtubeSvc.js
+++ app/scripts/services/youtubeSvc.js
@@ -6,7 +6,7 @@ angular.module('com.inthetelling.story')
 			domain: "http://gdata.youtube.com/",
 			timeout: 5000
 		};
-		svc.getVideoMetaDataV1 = function (id) {
+		var getVideoMetaDataV1 = function (id) {
 			var resourcePath = "feeds/api/videos/";
 			var queryString = "?v=2&alt=json&callback=JSON_CALLBACK";
 			var url = config.domain + resourcePath + id + queryString;
@@ -36,10 +36,42 @@ angular.module('com.inthetelling.story')
 			}
 			return true;
 		};
+		svc.getVideoData = function (id) {
+			var defer = $q.defer();
+			var promise = getVideoMetaDataV1(id);
+			promise.then(function (respData) {
+					var duration = 0;
+					var title = "";
+					var description = "";
+					if (checkNested(respData, "entry", "media$group", "yt$duration", "seconds")) {
+						duration = respData.entry.media$group.yt$duration.seconds;
+					}
+					if (checkNested(respData, "entry", "media$group", "media$title", "$t")) {
+						title = respData.entry.media$group.media$title.$t;
+					}
+					if (checkNested(respData, "entry", "media$group", "media$description", "$t")) {
+						description = respData.entry.media$group.media$description.$t;
+					}
+					//shelter ourselves from youtube api changes, by adapting their return to an internal format
+					var videoMetadata = {};
+					videoMetadata.id = id;
+					videoMetadata.title = title;
+					videoMetadata.description = description;
+					videoMetadata.duration = duration;
+					defer.resolve(videoMetadata);
+				},
+				function (reason) {
+					console.log("Failed:" + reason);
+					defer.reject(reason);
+				});
+			return defer.promise;
+		};
+		
+
 
 		svc.getVideoDuration = function (id) {
 			var defer = $q.defer();
-			var promise = svc.getVideoMetaDataV1(id);
+			var promise = getVideoMetaDataV1(id);
 			promise.then(function (respData) {
 					var duration = 0;
 					console.log('respData - youtube', respData);
