5725fbff2dad67ce3b194a306ae2c85d41eff439
Matt Self <self.matt@gmail.com>
2015-05-08 11:05:47 -0600

    ask authSvc for the role for the narrative and the default product. then set it in appState so it is reflected in the ui

--- app/scripts/directives/ittNarrativeTimeline.js
+++ app/scripts/directives/ittNarrativeTimeline.js
@@ -2,7 +2,7 @@
 /* For now this is just a thin wrapper around the playerController */
 
 angular.module('com.inthetelling.story')
-	.directive('ittNarrativeTimeline', function ($routeParams, $timeout, dataSvc, appState, modelSvc, errorSvc) {
+	.directive('ittNarrativeTimeline', function ($routeParams, $timeout, dataSvc, appState, authSvc, modelSvc, errorSvc) {
 		return {
 			restrict: 'A',
 			replace: true,
@@ -10,30 +10,34 @@ angular.module('com.inthetelling.story')
 
 			link: function (scope) {
 				appState.init();
+				//        console.log('user', appState.user);
+				//appState.product = "player";
+				dataSvc.getNarrative($routeParams.narrativePath)
+					.then(function (narrative) {
+						appState.narrativeId = narrative._id;
+						scope.narrative = narrative;
+						var narrativeRole = authSvc.getRoleForNarrative(narrative._id);
+						var defaultProduct = authSvc.getDefaultProductForRole(narrativeRole);
+						appState.product = defaultProduct;
+						angular.forEach(narrative.timelines, function (timeline) {
+							// TODO remove this hack to work around i18n paths when the api is sorted
+							if (timeline.path === $routeParams.timelinePath ||
+								timeline.path.en === $routeParams.timelinePath) {
+								if (timeline.episode_segments[0]) {
 
-				appState.product = "player";
-				dataSvc.getNarrative($routeParams.narrativePath).then(function (narrative) {
-					appState.narrativeId = narrative._id;
-					scope.narrative = narrative;
-					angular.forEach(narrative.timelines, function (timeline) {
-						// TODO remove this hack to work around i18n paths when the api is sorted
-						if (timeline.path === $routeParams.timelinePath ||
-							timeline.path.en === $routeParams.timelinePath) {
-							if (timeline.episode_segments[0]) {
+									appState.episodeId = timeline.episode_segments[0].episode_id;
+									appState.episodeSegmentId = timeline.episode_segments[0]._id;
 
-								appState.episodeId = timeline.episode_segments[0].episode_id;
-								appState.episodeSegmentId = timeline.episode_segments[0]._id;
-
-								scope.showPlayer = true;
+									scope.showPlayer = true;
+								}
 							}
+						});
+						if (!appState.episodeId) {
+							errorSvc.error({
+								data: "Sorry, no episode was found in this timeline."
+							});
 						}
 					});
-					if (!appState.episodeId) {
-						errorSvc.error({
-							data: "Sorry, no episode was found in this timeline."
-						});
-					}
-				});
 
 			}
 		};
