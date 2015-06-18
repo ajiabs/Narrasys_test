1016742049d8193db5f20070f404010e1233c246
Daniel Beck <daniel@danielbeck.net>
2015-06-18 14:14:18 -0400

    rich text editor

--- app/scripts/directives/sxsInputI18n.js
+++ app/scripts/directives/sxsInputI18n.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('com.inthetelling.story')
-	.directive('sxsInputI18n', function (appState) {
+	.directive('sxsInputI18n', function (appState, $timeout, textAngularManager) {
 		return {
 			templateUrl: 'templates/producer/inputi18n.html',
 			scope: {
 				field: "=sxsInputI18n",
 				inputtype: "=inputtype"
 			},
-			link: function (scope) {
+			link: function (scope, el, attrs) {
 				if (!scope.field) {
 					// need to init the object if it's empty
 					scope.field = {
@@ -16,6 +16,18 @@ angular.module('com.inthetelling.story')
 					};
 				}
 				scope.appState = appState;
+				console.log(attrs, textAngularManager);
+				if (scope.inputtype === 'textarea') {
+					// textAngular needs some extra magic for autofocus:
+					scope.textangularname = "ta" + new Date().getUTCMilliseconds() + "y" + Math.random().toString(16);
+
+					$timeout(function () { // wait for DOM
+						if (attrs.autofocus !== undefined) {
+							var editorScope = textAngularManager.retrieveEditor(scope.textangularname).scope;
+							editorScope.displayElements.text.trigger('focus');
+						}
+					});
+				}
 			}
 		};
 	});
