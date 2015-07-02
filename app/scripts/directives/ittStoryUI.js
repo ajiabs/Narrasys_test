e9f6c19983e93a95cb12ab6352420d8583250034
Daniel Beck <daniel@danielbeck.net>
2015-07-02 17:53:58 -0400

    more

--- app/scripts/directives/ittStoryUI.js
+++ app/scripts/directives/ittStoryUI.js
@@ -42,7 +42,10 @@ angular.module('com.inthetelling.story')
 				scope.hideToolbarTimer = $timeout(scope.hideToolbars, 3000);
 			};
 
-			$rootScope.$on('userKeypress.ESC', scope.hideToolbars);
+			$rootScope.$on('userKeypress.ESC', function () {
+				console.log("stoyUI got ESC");
+				scope.hideToolbars();
+			});
 			$rootScope.$on('userKeypress.SPACE', scope.showToolbars);
 			$rootScope.$on('lockToolbars', scope.lockToolbars);
 			$rootScope.$on('unlockToolbars', scope.unlockToolbars);
@@ -128,7 +131,7 @@ angular.module('com.inthetelling.story')
 	};
 })
 
-.directive('ittTopPane', function ($rootScope) {
+.directive('ittTopPane', function ($rootScope, $timeout) {
 	return {
 		restrict: 'EA',
 		transclude: true,
@@ -138,37 +141,88 @@ angular.module('com.inthetelling.story')
 		templateUrl: 'templates/v2/ui/top-pane.html',
 		link: function (scope, element) {
 			console.log("ittTopPane", scope);
-
 			scope.pane = {
 				active: false
 			};
-
 			$rootScope.$on('showPane', function (emit, label, evt) {
 				if (label === scope.label) {
-					scope.pointer = {
-						x: evt.clientX,
-						y: evt.clientY
-					};
-
-					console.log(element.width(), angular.element(window).width());
-
+					// TODO watch evt.currentTarget.offsetLeft and update position
+					if (evt) {
+						$timeout(function () {
+							scope.reposition({
+								x: evt.clientX,
+								y: evt.clientY
+							});
+						});
+					}
 					scope.showPane();
 				}
-				$rootScope.$on('userKeypress.ESC', scope.hidePane);
+			});
+			$rootScope.$on('userKeypress.ESC', function () {
+				scope.hidePane();
+				scope.$digest();
 			});
 
 			scope.hidePane = function () {
-				console.log("ATTEMPTING TO HIDE PANE");
 				scope.pane.active = false;
 				$rootScope.$emit('unlockToolbars');
 			};
 			scope.showPane = function () {
 				console.log("SCOPING PANE");
 				scope.pane.active = true;
-				element.removeClass('hidden');
 				$rootScope.$emit('lockToolbars');
+			};
+
+			scope.reposition = function (pointer) {
+				// For now, this is for top panels only. TODO extend to bottom as well?
+
+				// oh FFS no I am not going to  create separate pane directives and a controller just to get access to a couple of goddamn dom nodes
+				scope.paneNode = element.find('.pane'); // JQUERY DEPENDENCY
+				scope.pointerNode = element.find('.pointer');
+				console.log("REPOSITIONING: ", element, scope.paneNode);
+
+				if (pointer && scope.paneNode && scope.paneNode.width() > 0) {
+					var paneWidth = scope.paneNode.width();
+					if (paneWidth > pointer.x) {
+						console.log("Positioning pane on left");
+						scope.paneNode.css({
+							left: 0,
+							right: 'auto'
+						});
+					} else if (paneWidth > (window.innerWidth - pointer.x)) {
+						console.log("Positioning pane on right");
+						scope.paneNode.css({
+							left: 'auto',
+							right: 0
+						});
+					} else {
+						console.log("Centering pane");
+						scope.paneNode.css({
+							'left': (pointer.x - (paneWidth / 2)) + 'px',
+							'right': 'auto'
+						});
+					}
+
+					console.log("TTTTT", scope.paneNode, scope.paneNode.offsetTop);
+					var pointerPos = {
+						left: pointer.x,
+						top: (scope.paneNode.offset().top - 24)
+					};
+					if (pointerPos.left < 50) {
+						pointerPos.left = 50;
+					}
+					if (pointerPos.left > window.innerWidth - 50) {
+						pointerPos.left = window.innerWidth - 50;
+					}
+
+					scope.pointerNode.css(pointerPos);
+
+				}
+
 			};
 
 		}
 	};
-});
+})
+
+;
