adc65195e5eeb6c2d7b0c42e12a1343c4daeddd5
Daniel Beck <daniel@danielbeck.net>
2015-07-08 10:25:21 -0400

    cheap hackery to work around karma error in textAngular

---
+++ app/bower_components/textAngularRangyFake.js
@@ -0,0 +1,14 @@
+"use strict";
+/* stub to prevent textAngular from throwing errors in tests */
+
+window.rangy = {
+	init: function (x) {
+		return x;
+	},
+	saveSelection: function (x) {
+		return x;
+	},
+	restoreSelection: function (x) {
+		return x;
+	}
+};
