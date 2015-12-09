1874dba66dccb39ad8672aec3ee2a5d08c1bc216
githop <tom@githop.com>
2015-12-08 17:04:05 -0700

    adds - <itt-iframe> directive
    —accepts item object
    —conditionally applies sandbox attributes to non-pdf events/items.

---
+++ app/scripts/directives/ittIframe.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * Created by githop on 12/8/15.
+ */
+
+(function(){
+	angular.module('com.inthetelling.story')
+		.directive('ittIframe', ittIframe);
+
+	function ittIframe($compile) {
+		return {
+			restrict: 'EA',
+			scope: {
+				item: '='
+			},
+			link: link
+		};
+
+		function getTemplate(itemObj) {
+			var wSandbox = '<iframe src="{{item.souce}}" sandbox="allow-forms allow-same-origin allow-scripts"></iframe>';
+			var noSandbox = '<iframe src="{{item.source}}"></iframe>';
+			var template;
+
+			console.log('item passed to directive', itemObj);
+
+			if (itemObj.type === 'Link' && itemObj.url.match(/.pdf/)) {
+				template = noSandbox;
+				itemObj.source = itemObj.url;
+			} else {
+				template = wSandbox;
+				itemObj.source = itemObj.url;
+			}
+
+			return template;
+		}
+
+		function link(scope, element) {
+			element.html(getTemplate(scope.item)).show();
+			$compile(element.contents())(scope);
+		}
+
+	}
+})();
