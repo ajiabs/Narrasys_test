74b680e7be92c7664f41a099aaa0b5f86902c7e4
Daniel Beck <daniel@danielbeck.net>
2015-12-10 15:45:00 -0500

    Revert "jshint, mostly; included itt-iframe in more item templates"
    This reverts commit 29d7f2bae5162ecc6c6d5e69419afd79a3fbbf2b.

--- app/scripts/directives/ittIframe.js
+++ app/scripts/directives/ittIframe.js
@@ -1,35 +1,43 @@
-'use strict';
 /**
  *
  * Created by githop on 12/8/15.
  */
 
-angular.module('com.inthetelling.story')
-	.directive('ittIframe', function ($compile) {
+(function(){
+	angular.module('com.inthetelling.story')
+		.directive('ittIframe', ittIframe);
 
-		var getTemplate = function (itemObj) {
-			var wSandbox = '<iframe src="{{item.souce}}" sandbox="allow-forms allow-same-origin allow-scripts"></iframe>';
-			var noSandbox = '<iframe src="{{item.source}}"></iframe>';
-			var template;
-
-			// console.log('item passed to directive', itemObj);
-			if (itemObj.type === 'Link' && itemObj.url.match(/.pdf/)) {
-				template = noSandbox;
-			} else {
-				template = wSandbox;
-			}
-			itemObj.source = itemObj.url;
-			return template;
-		};
-
+	function ittIframe($compile) {
 		return {
 			restrict: 'EA',
 			scope: {
 				item: '='
 			},
-			link: function (scope, element) {
-				element.html(getTemplate(scope.item)).show();
-				$compile(element.contents())(scope);
-			}
+			link: link
 		};
-	});
+
+		function getTemplate(itemObj) {
+			var wSandbox = '<iframe src="{{item.souce}}" sandbox="allow-forms allow-same-origin allow-scripts"></iframe>';
+			var noSandbox = '<iframe src="{{item.source}}"></iframe>';
+			var template;
+
+			console.log('item passed to directive', itemObj);
+
+			if (itemObj.type === 'Link' && itemObj.url.match(/.pdf/)) {
+				template = noSandbox;
+				itemObj.source = itemObj.url;
+			} else {
+				template = wSandbox;
+				itemObj.source = itemObj.url;
+			}
+
+			return template;
+		}
+
+		function link(scope, element) {
+			element.html(getTemplate(scope.item)).show();
+			$compile(element.contents())(scope);
+		}
+
+	}
+})();
