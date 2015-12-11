4238c51f4d95a9b9cca0c313600f511a9b16d226
Daniel Beck <daniel@danielbeck.net>
2015-12-11 08:47:12 -0500

    alternate approach to itt-iframe

--- app/scripts/directives/ittIframe.js
+++ app/scripts/directives/ittIframe.js
@@ -3,7 +3,7 @@
  * Created by githop on 12/8/15.
  */
 
-(function(){
+(function () {
 	'use strict';
 
 	angular.module('com.inthetelling.story')
@@ -11,43 +11,56 @@
 
 	function ittIframe() {
 		return {
-			restrict: 'EA',
+			restrict: 'E',
 			scope: {
-				item: '='
+				src: '@',
+				contenttype: '@'
 			},
 			replace: true,
-			template: '<iframe ng-src="{{ctrl.item.url || ctrl.item.asset.url}}" ng-attr-sandbox="{{ctrl.sandbox}}"></iframe>',
-			controller: iframeCtrl,
-			controllerAs: 'ctrl',
-			bindToController: true
-		};
+			templateUrl: 'templates/iframe.html',
+			link: function (scope) {
+				// moved this all back out of the controller to avoid leaking $scope.sandbox across directives
+				var _sandboxAttrs = 'allow-forms allow-same-origin allow-scripts';
 
-		function iframeCtrl() {
-			var ctrl = this; // jshint ignore:line
+				scope.watcher = scope.$watchGroup(['src', 'contenttype'], function () {
+					if (!scope.src) {
+						return;
+					}
 
-			var _sandboxAttrs = 'allow-forms allow-same-origin allow-scripts';
+					if (scope.contenttype) {
+						// Uploads will always have a content type; we can trust it:
+						scope.sandbox = (scope.contenttype === 'text/html') ? _sandboxAttrs : undefined;
+					} else {
+						// no content type for links, so we have to fall back on string matching.
+						// Ideally we'd only apply the sandbox to html files, but that's hard to match, so for now we'll 
+						// default to sandbox unless proven otherwise.
+						scope.sandbox = _sandboxAttrs;
 
-			//root item's URL is a PDF
-			if (ctrl.item.type === 'Link' && ctrl.item.url.match(/.pdf/)) {
-				ctrl.sandbox = undefined;
+						// Remove it for PDFs (for now; probably we'll be growing this list later on)
+						if (scope.src.match(/.pdf$/)) {
+							delete scope.sandbox;
+						}
 
-				//it could be an uploaded asset, with its own URL
-			} else if (ctrl.item.type === 'Link' && angular.isDefined(ctrl.item.asset)) {
+						// Looks like we have some episodes where production used Links item types to point to asset uploads, 
+						// because upload templates  were broken (see TS-839). Would have been nice if they'd reported that 
+						// so we could fix it, but they didn't, so we'll clean up after them.
 
-				//uploaded PDF asset
-				if (ctrl.item.asset.content_type === 'application/pdf') {
-					ctrl.sandbox = undefined;
-				}
+						// These won't have a file extension to match on, so we'll have to just 
+						// assume that anything in our upload space won't have a framebreaker.
+						// URLs are either https://s3.amazonaws.com/ITT_Assets or https://s3.amazonaws.com/itt.user.uploads
 
-				//uploaded IMG asset
-				if (ctrl.item.asset._type === 'Asset::Image') {
-					ctrl.sandbox = _sandboxAttrs;
-				}
+						// TODO check to see if they always used ITT_Assets -- if so we can still protect against end-user uploads 
+						// of framebreaking files by using .match(/amazonaws.com\/ITT/) instead
+						if (scope.src.match(/amazonaws.com\/itt/i)) {
+							delete scope.sandbox;
+						}
+					}
+				});
 
-			} else {
-				//root item's URL is not a PDF ->sandbox it!
-				ctrl.sandbox = _sandboxAttrs;
+				scope.$on('$destroy', function () {
+					scope.watcher();
+				});
 			}
-		}
+		};
 	}
 })();
