a25e47986e738a093d9c4e884016167d87c6027f
githop <tom@githop.com>
2016-01-07 16:41:15 -0700

    chore(TS-756): swap out UUID function with prior art from awsSvc

--- app/scripts/services/YouTubePlayerManager.svc.js
+++ app/scripts/services/YouTubePlayerManager.svc.js
@@ -256,12 +256,13 @@
 		}
 
 		function _guid() {
-			function s4() {
-				return Math.floor((1 + Math.random()) * 0x10000)
-					.toString(16)
-					.substring(1);
-			}
-			return s4() + s4();
+			var d = new Date().getTime();
+			var uuid = 'xxxxxxxx'.replace(/[xy]/g, function (c) {
+				var r = (d + Math.random() * 16) % 16 | 0;
+				d = Math.floor(d / 16);
+				return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+			});
+			return uuid;
 		}
 
 		function setPlayerId(id, mainPlayer) {
