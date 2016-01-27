394c0c582ea9aad4d9d5df4f0e7ba935c4f6fba3
githop <tom@githop.com>
2016-01-27 13:02:19 -0700

    hotfix(TS-891): pass cb to onPlaybackQualityChange

--- app/scripts/controllers/VideoController.js
+++ app/scripts/controllers/VideoController.js
@@ -89,6 +89,7 @@ angular.module('com.inthetelling.story')
 
 		function onPlaybackQualityChange(state) {
 			if (state.data === 'medium' && /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor)) {
+				console.log('we made it!!!!');
 				youTubePlayerManager.setPlaybackQuality($scope.videoNode.id, "large");
 			}
 		}
