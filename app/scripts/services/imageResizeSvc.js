71112676879cfdb0cbe37ee508ac7ac2917ce5e4
githop <tom@githop.com>
2016-03-24 17:01:07 -0600

    chore(add-docs): fix typo

--- app/scripts/services/imageResizeSvc.js
+++ app/scripts/services/imageResizeSvc.js
@@ -247,7 +247,7 @@
 		 * @methodOf iTT.service:imageResize
 		 * @name _calculateNewDimensions
 		 * @description
-		 * Calculates the dimensions of a rectangle (for an image while maintaining the original aspect ratio.
+		 * Calculates the dimensions of a rectangle (for an image) while maintaining the original aspect ratio.
 		 * @param {Number} srcWidth Input width of rectangle.
 		 * @param {Number} srcHeight Input height of rectangle.
 		 * @param {Number} maxWidth Target width of rectangle.
