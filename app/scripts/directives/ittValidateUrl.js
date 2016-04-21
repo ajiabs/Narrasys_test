5e4a64a3649b20c04cc12f64b5bc5b6686cfad1e
githop <tom@githop.com>
2016-04-21 16:35:58 -0600

    updates(TS-772-774): todo --figure out how to remove not valid message for youtube URLs

--- app/scripts/directives/ittValidateUrl.js
+++ app/scripts/directives/ittValidateUrl.js
@@ -13,20 +13,8 @@
 	    return {
 			require: '?ngModel',
 			link: function link(scope, elm, attrs, ngModel) {
-				console.log('directive loaded!!', ngModel);
 				function allowSchemelessUrls() {
-					// Match Django's URL validator, which allows schemeless urls.
 					var URL_REGEXP = /^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
-
-					// Silently prefixes schemeless URLs with 'http://' when
-					// converting a view value to model value.
-					ngModel.$parsers.unshift(function(value) {
-						if (!URL_REGEXP.test(value) && URL_REGEXP.test('http://' + value)) {
-							return 'http://' + value;
-						} else {
-							return value;
-						}
-					});
 
 					ngModel.$validators.url = function(value) {
 						return ngModel.$isEmpty(value) || URL_REGEXP.test(value);
@@ -34,7 +22,6 @@
 				}
 
 				if (ngModel && attrs.type === 'url') {
-					console.log("all wired up!!");
 					allowSchemelessUrls();
 				}
 			}
