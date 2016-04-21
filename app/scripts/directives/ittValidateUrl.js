abf4cf9fd7aa1fe919c78be5057f6c21a1c0bddd
githop <tom@githop.com>
2016-04-21 16:51:09 -0600

    updates(TS-772-774): slightly better handling of youtube urls

--- app/scripts/directives/ittValidateUrl.js
+++ app/scripts/directives/ittValidateUrl.js
@@ -9,15 +9,20 @@
 	angular.module('com.inthetelling.story')
 		.directive('ittValidateUrl', ittValidateUrl);
 
-	function ittValidateUrl() {
+	function ittValidateUrl(youtubeSvc) {
 	    return {
 			require: '?ngModel',
 			link: function link(scope, elm, attrs, ngModel) {
+				console.log("ngModel!", ngModel);
 				function allowSchemelessUrls() {
 					var URL_REGEXP = /^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
 
 					ngModel.$validators.url = function(value) {
-						return ngModel.$isEmpty(value) || URL_REGEXP.test(value);
+						if (!youtubeSvc.isYoutubeUrl(value)) {
+							return ngModel.$isEmpty(value) || URL_REGEXP.test(value);
+						} else {
+							return value;
+						}
 					};
 				}
 
