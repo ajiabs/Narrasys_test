951c8f82b1763a41d7885e09cf143bca70a4a625
githop <tom@githop.com>
2016-04-22 09:29:49 -0600

    updates(TS-772-774): put validation logic in ittUtils

--- app/scripts/directives/ittValidateUrl.js
+++ app/scripts/directives/ittValidateUrl.js
@@ -9,17 +9,16 @@
 	angular.module('com.inthetelling.story')
 		.directive('ittValidateUrl', ittValidateUrl);
 
-	function ittValidateUrl(youtubeSvc) {
+	function ittValidateUrl(youtubeSvc, ittUtils) {
 	    return {
 			require: '?ngModel',
 			link: function link(scope, elm, attrs, ngModel) {
 				console.log("ngModel!", ngModel);
 				function allowSchemelessUrls() {
-					var URL_REGEXP = /^((?:http)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
 
 					ngModel.$validators.url = function(value) {
 						if (!youtubeSvc.isYoutubeUrl(value)) {
-							return ngModel.$isEmpty(value) || URL_REGEXP.test(value);
+							return ngModel.$isEmpty(value) || ittUtils.isValidURL(value);
 						} else {
 							return value;
 						}
