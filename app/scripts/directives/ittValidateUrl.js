366e08b3ad051c08af6532b5f1a985a03ed35edf
githop <tom@githop.com>
2016-04-22 13:58:04 -0600

    chore(TS-772-774): clean up logic for async validator

--- app/scripts/directives/ittValidateUrl.js
+++ app/scripts/directives/ittValidateUrl.js
@@ -16,43 +16,37 @@
 				item: '='
 			},
 			link: function link(scope, elm, attrs, ngModel) {
-
 				function allowSchemelessUrls() {
+					ngModel.$asyncValidators.url = function(modelVal) {
+						return $q(function(resolve, reject) {
+							if (modelVal.match(/^http:\/\//)) {
+								errorSvc.notify('Link Embed is disabled because ' + modelVal +' is not HTTPS');
+								resolve();
+							}
 
-					ngModel.$asyncValidators.url = function(modelVal, viewVal) {
-
-						if ( modelVal.match(/^http:\/\//)) {
-							console.log('rejected!!');
-							errorSvc.notify('Link Embed is disabled because ' + modelVal +' is not HTTPS');
-							return $q(function(resolve) {resolve();});
-						}
-
-						if (!youtubeSvc.isYoutubeUrl(modelVal) && ittUtils.isValidURL(modelVal)) {
-							var itemUrl = modelVal;
-							return dataSvc.checkXFrameOpts(itemUrl)
-							.then(function(noEmbed) {
-								var xFrameOptsNote = ' does not allow embedding, so this link will open in a new tab';
-								scope.item.noEmbed = noEmbed;
-								if (noEmbed) {
-									scope.item.tipText = 'Link embed is disabled because ' + itemUrl + ' does not allow iframing';
-									scope.item.showInlineDetail = false;
-									errorSvc.notify(itemUrl + xFrameOptsNote);
-								}
-								return modelVal;
-							});
-						} else {
-							return $q(function(resolve, reject) {
+							if (!youtubeSvc.isYoutubeUrl(modelVal) && ittUtils.isValidURL(modelVal)) {
+								var itemUrl = modelVal;
+								dataSvc.checkXFrameOpts(itemUrl)
+									.then(function(noEmbed) {
+										var xFrameOptsNote = ' does not allow embedding, so this link will open in a new tab';
+										scope.item.noEmbed = noEmbed;
+										if (noEmbed) {
+											scope.item.tipText = 'Link embed is disabled because ' + itemUrl + ' does not allow iframing';
+											scope.item.showInlineDetail = false;
+											errorSvc.notify(itemUrl + xFrameOptsNote);
+										}
+										resolve(modelVal);
+									});
+							} else {
 								if (ngModel.$isEmpty(modelVal) || ittUtils.isValidURL(modelVal)) {
-									console.log("we must be here!!", modelVal);
 									scope.item.noEmbed = false;
 									scope.item.tipText = undefined;
 									resolve(modelVal);
 								} else {
-									console.log('hmm', modelVal);
 									reject(modelVal);
 								}
-							});
-						}
+							}
+						});
 					};
 				}
 
