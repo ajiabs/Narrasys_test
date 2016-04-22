438db9c92cba6adb34ea38b0cd24ec1aa527f740
githop <tom@githop.com>
2016-04-22 09:22:03 -0600

    updates(TS-772-774): no FTP in regex

--- app/scripts/directives/ittValidateUrl.js
+++ app/scripts/directives/ittValidateUrl.js
@@ -15,7 +15,7 @@
 			link: function link(scope, elm, attrs, ngModel) {
 				console.log("ngModel!", ngModel);
 				function allowSchemelessUrls() {
-					var URL_REGEXP = /^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
+					var URL_REGEXP = /^((?:http)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
 
 					ngModel.$validators.url = function(value) {
 						if (!youtubeSvc.isYoutubeUrl(value)) {
