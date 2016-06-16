b8d3fd1e049903bfea2ea6e73967ab9c2b5b6765
githop <tom@githop.com>
2016-06-16 09:51:26 -0600

    updates(route loading spinner): only show loader for load times greater than 500ms

--- app/scripts/directives/ittRouteLoading.js
+++ app/scripts/directives/ittRouteLoading.js
@@ -20,28 +20,35 @@
 				'	</div><span class="loading__text">Loading</span>',
 				'</div>'
 			].join(' '),
-			controller: ['$rootScope', '$scope', 'errorSvc', function($rootScope, $scope, errorSvc) {
+			controller: function($rootScope, $scope, $timeout, ittUtils, errorSvc) {
 				var ctrl = this;
 				ctrl.isLoading = false;
+				var threshold;
 
+				//only show timer if loading time is longer than 500ms
 				$rootScope.$on('$routeChangeStart', function() {
-					ctrl.isLoading = true;
+					threshold = $timeout(function() {
+						ctrl.isLoading = true;
+					}, 500)
+
 				});
 
 				$rootScope.$on('$routeChangeSuccess', function() {
+					$timeout.cancel(threshold);
 					ctrl.isLoading = false;
 				});
 
 				//do not show loading indicator
 				//when we have errors
 				$scope.$watch(function() {
-					return errorSvc.errors.length;
+					return errorSvc.errors.length
 				}, function(newVal, oldVal) {
 					if (newVal !== oldVal && newVal > 0) {
+						$timeout.cancel(threshold);
 						ctrl.isLoading = false;
 					}
 				});
-			}],
+			},
 			controllerAs: 'routeLoadingCtrl',
 			bindToController: true
 	    };
