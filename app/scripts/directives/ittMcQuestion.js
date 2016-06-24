054570ab90d530960f1381602b1a17961aeb6381
githop <tom@githop.com>
2016-06-24 16:19:00 -0600

    chore(whoops): remove mistakenly added scope binding

--- app/scripts/directives/ittMcQuestion.js
+++ app/scripts/directives/ittMcQuestion.js
@@ -10,7 +10,7 @@ angular.module('com.inthetelling.story')
 				qid: '=',
 				choices: '=',
 				correct: '=',
-				onEpisodeSelect: '=',
+				onChoice: '=',
 				questionType: '@',
 				showChart: '='
 			},
