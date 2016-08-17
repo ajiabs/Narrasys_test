56143434bd27a72457612dccd83756acf40e8727
githop <tom@githop.com>
2016-08-17 11:32:14 -0600

    build v4.8.59-dev-17

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,6 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-const exec = require('child_process').exec;
-
 module.exports = function (grunt) {
 	require('load-grunt-tasks')(grunt);
 	require('time-grunt')(grunt);
