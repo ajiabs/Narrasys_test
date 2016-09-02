b070703be59d0bf97ad1e1dd77f8484b87102240
githop <tom@githop.com>
2016-09-02 14:56:35 -0600

    updates(TS-1060): default timestamp text --ittTimestampSelect - show 'default' on episode tab, 'inherit' on layout or item.

--- app/scripts/directives/ittTimestampSelect.js
+++ app/scripts/directives/ittTimestampSelect.js
@@ -1,29 +1,30 @@
 /**
  * Created by githop on 6/30/16.
  */
-(function() {
+(function () {
 	'use strict';
 
 	angular.module('com.inthetelling.story')
 		.directive('ittTimestampSelect', ittTimestampSelect);
 
 	function ittTimestampSelect() {
-	    return {
-	        restrict: 'EA',
-	        scope: true,
+		return {
+			restrict: 'EA',
+			scope: true,
 			template: [
-			'<div class="field">',
-			'	<div class="label"><span ng-if="episode">Display </span>Timestamp</div>',
-			'	<div class="input">',
-			'		<select size="1" ng-model="itemForm.timestamp">',
-			'			<option value="">(Default)</option>',
-			'			<option value="None">No timestamp</option>',
-			'			<option value="Inline">Inline timestamp</option>',
-			'		</select>',
-			'	</div>',
-			'</div>'
+				'<div class="field">',
+				'	<div class="label"><span ng-if="episode">Display </span>Timestamp</div>',
+				'	<div class="input">',
+				'		<select size="1" ng-model="itemForm.timestamp">',
+				'			<option ng-if="episode" value="">(Default)</option>',
+				'			<option ng-if="item" value="">(Inherit)</option>',
+				'			<option value="None">Off</option>',
+				'			<option value="Inline">On</option>',
+				'		</select>',
+				'	</div>',
+				'</div>'
 			].join(' ')
-	    };
+		};
 	}
 
 
