4d95badfa2d26400bf774568d3cd4547b54f41aa
githop <tom@githop.com>
2016-09-23 10:28:36 -0600

    updates(TS-1121): create ittNav directive to handle common nav functions - begin implementation updates(icons): test out creating new icon set and using it in project as asset

---
+++ app/scripts/directives/ittNav.js
@@ -0,0 +1,34 @@
+/**
+ * Created by githop on 9/22/16.
+ */
+(function() {
+	'use strict';
+
+	angular.module('com.inthetelling.story')
+		.directive('ittNav', ittNav);
+
+	function ittNav() {
+	    return {
+	        restrict: 'EA',
+			scope: {
+	        	onLogout: '&'
+			},
+			template: [
+				'<div class="ancillaryNav">',
+				'<pre>{{$ctrl.currentPath }}</pre>',
+				'	<a class="button" ng-if="$ctrl.currentPath !== \'/stories\'" href="/#/stories">Narratives</a>',
+				'	<a class="button" href="/#/account">Account</a>',
+				'	<a class="button" ng-if="$ctrl.currentPath !== \'/projects\'" href="/#/projects">Projects</a>',
+				'	<a class="button" ng-click="$ctrl.onLogout()">Log out</a>',
+				'</div>'
+			].join('\n'),
+			controller: ['$location', function($location) {
+				this.currentPath = $location.path();
+			}],
+			controllerAs: '$ctrl',
+			bindToController: true
+	    };
+	}
+
+
+})();
