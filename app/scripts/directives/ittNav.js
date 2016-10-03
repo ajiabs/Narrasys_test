1b9255e2a483afcb8f6c49393254c6947362a35e
githop <tom@githop.com>
2016-10-03 10:18:07 -0600

    updates(TS-1124) nav bar --ittNav - add inPlayer boolean --colors.css - put nav styles into here so they are available outside of the projects/narratives pages --narrative.css - space between image and name in nav --player.html - set nav to in-player mode.

--- app/scripts/directives/ittNav.js
+++ app/scripts/directives/ittNav.js
@@ -11,13 +11,14 @@
 	    return {
 	        restrict: 'EA',
 			scope: {
-	        	onLogout: '&'
+	        	onLogout: '&',
+				inPlayer: '=?'
 			},
 			template: [
 				'<div>',
 				'	<header class="nav__header">',
 				'		<div class="nav__item">',
-				'			<img src="images/Narrasys_Banner.svg"/>',
+				'			<img ng-style="{width: $ctrl.inPlayer ? \'75%\' : \'100%\' }" src="images/Narrasys_Banner.svg"/>',
 				'		</div>',
 				'		<div class="nav__item" ng-if="$ctrl.appState.user._id">',
 				'			<div class="nav__controls">',
@@ -26,8 +27,8 @@
 				'						<img ng-src="{{$ctrl.appState.user.avatar || \'/images/no-avatar.gif\'}}">',
 				'						<a class="nav__button" ng-if="$ctrl.appState.user">{{$ctrl.appState.user.name}}</a>',
 				'					</div>',
-				'					<a class="nav__button" ng-if="$ctrl.currentPath !== \'/projects\'" href="/#/projects">My Projects</a>',
-				'					<a class="nav__button" ng-if="$ctrl.currentPath !== \'/stories\'" href="/#/stories">My Narratives</a>',
+				'					<a class="nav__button" ng-if="$ctrl.currentPath !== \'/projects\' && !$ctrl.inPlayer" href="/#/projects">My Projects</a>',
+				'					<a class="nav__button" ng-if="$ctrl.currentPath !== \'/stories\' && !$ctrl.inPlayer" href="/#/stories">My Narratives</a>',
 				'				</div>',
 				'				<div class="navControls__item item--right">',
 				'					<a class="nav__button" ng-click="$ctrl.onLogout()">Log out</a>',
