5737bf1e3104b24db3dbcee8b60fa733262bbcd8
githop <tom@githop.com>
2016-10-07 12:11:48 -0600

    updates(TS-1148): accurate font color - pro templates --cssGen.ts - include primary font in header mixin --transmedia templates- apply 'item__title' class to required viewing

--- utils/episode-styles-generator/lib/cssGen.ts
+++ utils/episode-styles-generator/lib/cssGen.ts
@@ -152,6 +152,7 @@ export const genCss = ({nameSpace, accentColor, primaryColor, secondaryColor, li
 
     @mixin ${nameSpace}-header {
     ${fontFam(headerFont)};
+    	color: $${nameSpace}Primary;
     }
     @mixin ${nameSpace}-body {
     ${fontFam(bodyFont)};
