562e521a195c33aba68f5dec8945be8fbb830e68
githop <tom@githop.com>
2016-12-14 12:21:52 -0700

    fix(htmlGen.ts): fix footer class

--- utils/episode-styles-generator/lib/htmlGen.ts
+++ utils/episode-styles-generator/lib/htmlGen.ts
@@ -24,6 +24,14 @@ const handleBrandImg = (brandLogo:string, nameSpace:string) => {
 	return `<img class="professional__logo" src="images/customer/${brandLogo}"/>`
 };
 
+const handleFooterClass = (nameSpace:string) => {
+	if (nameSpace === 'unbranded') {
+		return `professional__branding branding footer`;
+	}
+
+	return 'professional__branding';
+};
+
 export const genHtml = (nameSpace:string, brandLogo:string = 'Narrasys_brand_logo.svg') => {
     return `
     <div class="episode professional ${nameSpace}" ng-class="episode.styleCss">
@@ -34,7 +42,7 @@ export const genHtml = (nameSpace:string, brandLogo:string = 'Narrasys_brand_log
 	<span ng-include="'templates/episode/components/video.html'"></span>
 	<span ng-include="'templates/episode/components/windowfg.html'"></span>
 	
-	<div ng-if="appState.viewMode != 'watch'" class="professional__branding">
+	<div ng-if="appState.viewMode != 'watch'" class="${handleFooterClass(nameSpace)}">
 		<div class="branding--content">
 			${handleCopyright(nameSpace)}
 			${handleBrandImg(brandLogo, nameSpace)}
