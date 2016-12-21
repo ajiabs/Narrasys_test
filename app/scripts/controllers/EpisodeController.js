5a3b5bb0e06538095625e226dc6a75d7a61c3667
githop <tom@githop.com>
2016-12-21 12:30:57 -0700

    chore(UofA template): clean up prototype code

--- app/scripts/controllers/EpisodeController.js
+++ app/scripts/controllers/EpisodeController.js
@@ -3,23 +3,4 @@
 angular.module('com.inthetelling.story')
 	.controller('EpisodeController', function ($scope) {
 
-
-		var uABgs = [
-			{img: 'UofA_Fullscreen_BG_Square_Trans.svg', name: 'UofA_Fullscreen_BG_Square_Trans'},
-			{img: 'UofA_Fullscreen_BG_Square.svg', name: 'UofA_Fullscreen_BG_Square'},
-			{img: 'UofA_Fullscreen_BG_Trans.svg', name: 'UofA_Fullscreen_BG_Trans'}
-		];
-
-
-		$scope.episodeBg = null;
-		$scope.currentImg = null;
-		$scope.uaBgs = uABgs;
-
-		var pathPrefix = '/images/customer/';
-
-		$scope.swapBgImage = swapBgImage;
-		function swapBgImage(bg) {
-			$scope.currentImg = bg.name;
-			$scope.episodeBg = {'background-image': 'url(' +pathPrefix + bg.img + ')' };
-		}
 	});
