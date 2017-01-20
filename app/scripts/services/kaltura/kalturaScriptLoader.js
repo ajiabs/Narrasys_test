776af39a04eb965c8bf2b79fb7de4dde57ff7621
githop <tom@githop.com>
2017-01-20 11:38:00 -0700

    updates(kaltura): improve method for resolving scriptloader load

--- app/scripts/services/kaltura/kalturaScriptLoader.js
+++ app/scripts/services/kaltura/kalturaScriptLoader.js
@@ -9,21 +9,26 @@
 	angular.module('com.inthetelling.story')
 		.factory('kalturaScriptLoader', kalturaScriptLoader);
 
-	function kalturaScriptLoader($q, $timeout) {
+	function kalturaScriptLoader($q) {
 		return {
 			load: load
 		};
 
 		function load(partnerId, uiConfId) {
 			return $q(function(resolve){
-				var src = 'https://cdnapisec.kaltura.com/p/' + partnerId + '/sp/' + partnerId + '00/embedIframeJs/uiconf_id/' + uiConfId + '/partner_id/' + partnerId;
-				var tag = document.createElement('script');
-				tag.src = src;
-				tag.id = partnerId;
-				var firstScriptTag = document.getElementsByTagName('script')[0];
-				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-				$timeout(resolve, 900);
+				if (typeof(KWidget) == 'undefined') {
+					var src = 'https://cdnapisec.kaltura.com/p/' + partnerId + '/sp/' + partnerId + '00/embedIframeJs/uiconf_id/' + uiConfId + '/partner_id/' + partnerId;
+					var tag = document.createElement('script');
+					tag.src = src;
+					tag.id = partnerId;
+					var firstScriptTag = document.getElementsByTagName('script')[0];
+					firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+					tag.onload = resolve;
+				} else {
+					resolve();
+				}
+
 			});
 		}
 	}
