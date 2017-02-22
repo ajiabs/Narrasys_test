9998173bd98914011c3ccfe1863b42b6a42f2bfb
githop <tom@githop.com>
2017-02-22 14:16:15 -0700

    chore(code): remove tmp debug code.

--- app/scripts/directives/ittVideo.js
+++ app/scripts/directives/ittVideo.js
@@ -34,18 +34,12 @@ function ittVideo() {
 		ctrl.setCssClass = setCssClass;
 		ctrl.showMask = showMask;
 		ctrl.appState = appState;
-		ctrl.resetKaltura = resetKaltura;
 		var _existy = ittUtils.existy;
 
 		//controller event bindings
 		$scope.$on('$destroy', onDestroy);
 
 		onInit();
-
-		//for debugging only
-		function resetKaltura() {
-			playbackService.debugReset(ctrl.playerId);
-		}
 
 		function onInit() {
 			//empty input passed in
