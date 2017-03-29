a6ec21c585d895f5000bae8f2a6171a9d87914c7
githop <tom@githop.com>
2017-03-29 15:12:47 -0600

    chore(code-prep): showFocus, searchPanel format and add $inject array

--- app/scripts/directives/ittSearchPanel.js
+++ app/scripts/directives/ittSearchPanel.js
@@ -4,21 +4,25 @@
 // Maybe rename it after we finally migrate completely away from the three-modes layout.
 
 angular.module('com.inthetelling.story')
-	.directive('ittSearchPanel', function ($timeout, appState) {
-		return {
-			restrict: 'A',
-			replace: true,
-			scope: true,
-			templateUrl: 'templates/searchpanel.html',
-			controller: 'SearchPanelController',
-			link: function (scope) {
+  .directive('ittSearchPanel', ittSearchPanel);
 
-				scope.appState = appState;
+ittSearchPanel.$inject = ['appState'];
 
-				// $timeout(function () {
-				// console.log("ittSearchPanel", scope);
-				scope.indexEvents();
-				// });
-			}
-		};
-	});
+function ittSearchPanel(appState) {
+  return {
+    restrict: 'A',
+    replace: true,
+    scope: true,
+    templateUrl: 'templates/searchpanel.html',
+    controller: 'SearchPanelController',
+    link: function (scope) {
+
+      scope.appState = appState;
+
+      // $timeout(function () {
+      // console.log("ittSearchPanel", scope);
+      scope.indexEvents();
+      // });
+    }
+  };
+}
