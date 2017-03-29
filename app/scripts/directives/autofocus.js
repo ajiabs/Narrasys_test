e6b582907bcb45d302755059b67673fe5bb4d3ac
githop <tom@githop.com>
2017-03-29 14:33:43 -0600

    chore(code-prep): autofocus format and add $inject array

--- app/scripts/directives/autofocus.js
+++ app/scripts/directives/autofocus.js
@@ -2,16 +2,20 @@
 // attach to any directive to make its first input/textarea autofocus
 
 angular.module('com.inthetelling.story')
-	.directive('autofocus', function ($timeout) {
-		return {
-			link: function (scope, element) {
-				$timeout(function () { // give any child directives time to render themselves...
-					if (element[0].tagName === 'TEXTAREA' || element[0].tagName === 'INPUT') {
-						element[0].focus();
-					} else {
-						element.find('input,textarea')[0].focus();
-					}
-				});
-			}
-		};
-	});
+  .directive('autofocus', autofocus);
+
+autofocus.$inject = ['$timeout'];
+
+function autofocus($timeout) {
+  return {
+    link: function (scope, element) {
+      $timeout(function () { // give any child directives time to render themselves...
+        if (element[0].tagName === 'TEXTAREA' || element[0].tagName === 'INPUT') {
+          element[0].focus();
+        } else {
+          element.find('input,textarea')[0].focus();
+        }
+      });
+    }
+  };
+}
