cb821cf0d9fdc361a85d6d9bc9dc0e0161344391
githop <tom@githop.com>
2017-03-30 09:58:34 -0600

    updates(code prep): validationTip - whitespace format

--- app/scripts/directives/ittValidationTip.js
+++ app/scripts/directives/ittValidationTip.js
@@ -2,27 +2,27 @@
  * Created by githop on 8/24/16.
  */
 
-(function() {
-	'use strict';
+(function () {
+  'use strict';
 
-	angular.module('com.inthetelling.story')
-		.directive('ittValidationTip', ittValidationTip);
+  angular.module('com.inthetelling.story')
+    .directive('ittValidationTip', ittValidationTip);
 
-	function ittValidationTip() {
-	    return {
-	        restrict: 'EA',
-	        scope: {
-	        	text: '@',
-				doInfo: '=?'
-			},
-			template: [
-				'<span class="tooltip-static">',
-				'	<i class="fa fa-info-circle" ng-class="{\'error-red\': !doInfo, \'info-blue\': doInfo}"></i>',
-				'	<span class="tooltip-text">{{text}}</span>',
-				'</span>'
-			].join('\n')
-	    };
-	}
+  function ittValidationTip() {
+    return {
+      restrict: 'EA',
+      scope: {
+        text: '@',
+        doInfo: '=?'
+      },
+      template: [
+        '<span class="tooltip-static">',
+        '	<i class="fa fa-info-circle" ng-class="{\'error-red\': !doInfo, \'info-blue\': doInfo}"></i>',
+        '	<span class="tooltip-text">{{text}}</span>',
+        '</span>'
+      ].join('\n')
+    };
+  }
 
 
 })();
