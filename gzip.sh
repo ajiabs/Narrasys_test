56448104f29aaace7d7f638e295d0dce32ba1e47
githop <tom@githop.com>
2017-04-07 13:36:10 -0600

    updates(webpack config): do not overwrite privacy.html

--- gzip.sh
+++ gzip.sh
@@ -2,4 +2,4 @@
 
 find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 rm -rf dist/manifest.*
-cp app/privacy.html app/version.txt app/apple-touch-icon.png app/apple-touch-icon-72.png app/apple-touch-icon-114.png app/favicon.ico dist/
+cp app/version.txt app/apple-touch-icon.png app/apple-touch-icon-72.png app/apple-touch-icon-114.png app/favicon.ico dist/
