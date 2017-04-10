d4202274d59f0ec9100496e000f2cb77ce74fe0c
githop <tom@githop.com>
2017-04-10 13:33:09 -0600

    updates(wp-refactor): perf - replace twiddle scene with pure CSS

--- app/scripts/directives/ittScene.ts
+++ app/scripts/directives/ittScene.ts
@@ -58,57 +58,18 @@ export default function ittScene($interval, appState) {
       // scope.crossEpisodePath = appState.crossEpisodePath;
       // }
 
-      var twiddleScene = function () {
-        var magnetNode = element.find('.videoMagnet img');
-        if (magnetNode.height() === null) {
-          // console.warn("twiddleScene called with no visible video magnet; waiting.");
-          var unwatchMagnet = scope.$watch(function () {
-            // Don't try to optimize by using magnetNode from above; if we got here in the first place magnetNode is undefined.
-            // This is an expensive $watch but will only run for a tick or two while the scene is being drawn...
-            return element.find('.videoMagnet img').height();
-          }, function (newH) {
-            if (newH > 0) {
-              unwatchMagnet();
-              twiddleScene();
-            }
-          });
-        } else {
-          element.find('.matchVideoHeight:visible').each(function () {
-            $(this).css("height", element.find('.videoMagnet img').height());
-          });
-          var availableViewportHeight = angular.element(window).height() - $('#CONTAINER').scrollTop();
-          /* TOOLBAR HEIGHT */
-          element.find('.stretchToViewport:visible').each(function () {
-            $(this).css("min-height", (availableViewportHeight - $(this).offset().top));
-          });
-          // landing screen: keep the bottom toolbar onscreen
-          element.find('.stretchToViewportShort:visible').each(function () {
-            $(this).css("min-height", (availableViewportHeight - $(this).offset().top - 210)); // HARDCODED FOOTER HEIGHT
-          });
-        }
-
-        element.find('.content').each(function () {
-          var contentpane = $(this);
-          if (contentpane.outerWidth() > 550) {
-            contentpane.addClass('allowSidebars');
-          } else {
-            contentpane.removeClass('allowSidebars');
-          }
-        });
-      };
-
       // Trigger twiddleScene when the window changes size, the scene becomes current, or the viewMode changes:
-      scope.unwatch = scope.$watch(function () {
-        return {
-          winWidth: appState.windowWidth,
-          winHeight: appState.windowHeight,
-          newMode: appState.viewMode
-        };
-      }, function (the) {
-        if (the.newMode === 'discover' && scope.scene.isCurrent) {
-          twiddleScene();
-        }
-      }, true);
+      // scope.unwatch = scope.$watch(function () {
+      //   return {
+      //     winWidth: appState.windowWidth,
+      //     winHeight: appState.windowHeight,
+      //     newMode: appState.viewMode
+      //   };
+      // }, function (the) {
+      //   if (the.newMode === 'discover' && scope.scene.isCurrent) {
+      //     twiddleScene();
+      //   }
+      // }, true);
 
       // trigger init when the user edits content:
       scope.unwatchEdits = scope.$watch(function () {
@@ -117,13 +78,12 @@ export default function ittScene($interval, appState) {
 
       // HACK to catch cases (mostly on ios) where matchvideoheight isn't matching.
       // slow, odd interval
-      scope.safetyBelt = $interval(twiddleScene, 1321);
+      // scope.safetyBelt = $interval(twiddleScene, 1321);
 
       // cleanup watchers on destroy
       scope.$on('$destroy', function () {
-        scope.unwatch();
         scope.unwatchEdits();
-        $interval.cancel(scope.safetyBelt);
+        // $interval.cancel(scope.safetyBelt);
       });
 
     },
