b30e54ab72e93639a8bde7b34f68d20f49ca4c21
githop <tom@githop.com>
2017-04-14 16:40:21 -0600

    chore(build): allow gzip to happen upstream

--- gzip.sh
+++ gzip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf dist/manifest.*
-find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 mv dist/*.map sourcemaps/
 cp app/version.txt app/apple-touch-icon.png app/apple-touch-icon-72.png app/apple-touch-icon-114.png app/favicon.ico dist/
