ce2ff53cf5db59bf165391b6dbf9318bebb2ca1e
githop <tom@githop.com>
2017-04-17 11:45:58 -0600

    chore(format): use ES6 template for directive template

--- app/scripts/directives/ittUrlField.ts
+++ app/scripts/directives/ittUrlField.ts
@@ -13,22 +13,21 @@ export default function ittUrlField() {
       label: '@',
       onAttach: '&'
     },
-    template: [
-      '<div class="field">',
-      '	<div class="label">{{$ctrl.label || "URL"}}',
-      '	<span ng-repeat="(field, val) in $ctrl.validatedFields">',
-      '		<itt-validation-tip ng-if="val.showInfo" text="{{val.message}}" do-info="val.doInfo"></itt-validation-tip>',
-      '	</span>',
-      '	</div>',
-      '	<div class="input" ng-if="!$ctrl.videoOnly">',
-      '		<input type="text" name="itemUrl" ng-model="$ctrl.data.url" ng-focus="$ctrl.onFocus()" ng-model-options="{ updateOn: \'blur\' }"  itt-valid-item-url on-validation-notice="$ctrl.handleItemValidationMessage($notice)"/>',
-      '	</div>',
-      '	<div class="input" ng-if="$ctrl.videoOnly === true">',
-      '		<input type="text" ng-model="$ctrl.data" itt-valid-episode-url on-validation-notice="$ctrl.handleEpisodeValidationMessage($notice)"/>',
-      '		<button ng-if="$ctrl.data" ng-click="$ctrl.onAttach({$url: $ctrl.data})">Attach Video</button>',
-      '	</div>',
-      '</div>'
-    ].join(' '),
+    template: `
+      <div class="field">
+      	<div class="label">{{$ctrl.label || "URL"}}
+      	<span ng-repeat="(field, val) in $ctrl.validatedFields">
+      		<itt-validation-tip ng-if="val.showInfo" text="{{val.message}}" do-info="val.doInfo"></itt-validation-tip>
+      	</span>
+      	</div>
+      	<div class="input" ng-if="!$ctrl.videoOnly">
+      		<input type="text" name="itemUrl" ng-model="$ctrl.data.url" ng-focus="$ctrl.onFocus()" ng-model-options="{ updateOn: \'blur\' }"  itt-valid-item-url on-validation-notice="$ctrl.handleItemValidationMessage($notice)"/>
+      	</div>
+      	<div class="input" ng-if="$ctrl.videoOnly === true">
+      		<input type="text" ng-model="$ctrl.data" itt-valid-episode-url on-validation-notice="$ctrl.handleEpisodeValidationMessage($notice)"/>
+      		<button ng-if="$ctrl.data" ng-click="$ctrl.onAttach({$url: $ctrl.data})">Attach Video</button>
+      	</div>
+      </div>`,
     controller: ['$scope', function ($scope) {
       var ctrl = this;
       ctrl.handleItemValidationMessage = handleItemValidationMessage;
