73c6609ee9b5bb67bb0e734ca771165bad24328d
githop <tom@githop.com>
2017-06-30 12:16:21 -0600

    chore(style): tslint fixes

--- app/scripts/services/wistia/wistiaUrlService.ts
+++ app/scripts/services/wistia/wistiaUrlService.ts
@@ -3,7 +3,6 @@ import {IParsedMediaSrcObj, IUrlSubService} from '../../interfaces';
  * Created by githop on 4/12/17.
  */
 
-
 export interface IWistiaUrlservice extends IUrlSubService {
   extractId(input: string): string;
 }
@@ -12,7 +11,7 @@ export class WistiaUrlService implements IWistiaUrlservice {
   public type = 'wistia';
   public mimetype = `video/x-${this.type}`;
   private embedUrl = 'http://fast.wistia.net/embed/iframe/';
-  private parser = new DOMParser();
+  // private parser = new DOMParser();
 
   getMimeType() {
     return this.mimetype;
@@ -20,64 +19,63 @@ export class WistiaUrlService implements IWistiaUrlservice {
 
   parseMediaSrc(mediaSrcArr) {
     return mediaSrcArr.reduce((pmo: IParsedMediaSrcObj, mediaSrc: string) => {
-      if (this.isWistia(mediaSrc)) {
+      if (WistiaUrlService.isWistia(mediaSrc)) {
         pmo.mediaSrcArr.push(mediaSrc);
       }
       return pmo;
     }, {type: this.type, mediaSrcArr: []});
   }
 
-  //TODO: it looks like the embed code itself doesn't contain enough info to map it into a URL, need to know provider_url aka domain
+  //TODO: it looks like the embed code itself doesn't contain enough info to map it into a URL
+  // need to know provider_url aka domain
   parseInput(input: string) {
-    if (this.isWistia(input)) {
+    if (WistiaUrlService.isWistia(input)) {
       //not an embed code, must already be a URL
-      if (!this.isEmbed(input)) {
+      if (!WistiaUrlService.isEmbed(input)) {
         return input;
       }
-      return input
     }
   }
 
   extractId(input: string) {
     //simple url input for now
-    if (this.isWistia(input)) {
-      return this.parseIdfromUrl(input);
+    if (WistiaUrlService.isWistia(input)) {
+      return WistiaUrlService.parseIdfromUrl(input);
     }
   }
 
   canPlay(input) {
-    return this.isWistia(input);
+    return WistiaUrlService.isWistia(input);
   }
 
   getOutgoingUrl(url, startAt) {
     //use iframe embed url format.
-    let params = `?autoplay=true&time=${startAt}`;
-    let videoId = this.parseIdfromUrl(url);
-    let u = `${videoId}${params}`;
+    const params = `?autoplay=true&time=${startAt}`;
+    const videoId = WistiaUrlService.parseIdfromUrl(url);
+    const u = `${videoId}${params}`;
     return this.embedUrl + u;
   }
 
-  private parseIdFromEmbedCode(embedCode: string) {
-    let doc = this.parser.parseFromString(embedCode, 'text/html');
-    let classes = doc.getElementsByClassName('wistia_embed')[0].className;
-    return classes.split('_').pop().split('_').pop();
-  }
+  // private parseIdFromEmbedCode(embedCode: string) {
+  //   const doc = this.parser.parseFromString(embedCode, 'text/html');
+  //   const classes = doc.getElementsByClassName('wistia_embed')[0].className;
+  //   return classes.split('_').pop().split('_').pop();
+  // }
 
   //expand eventually to work with embed codes as well as URLs
-  private isWistia(url: string) {
+  private static isWistia(url: string) {
     const re: RegExp = /https?:\/\/(.+)?(wistia\.com|wi\.st)\/.*/;
     return re.test(url);
   }
 
-  private isEmbed(input: string) {
+  private static isEmbed(input: string) {
     return /<script/i.test(input);
   }
 
-  private parseIdfromUrl(url: string) {
+  private static parseIdfromUrl(url: string) {
     //id should be last in the path
-    let path = url.split('/');
-    return path[path.length -1];
+    const path = url.split('/');
+    return path[path.length - 1];
   }
 }
-
 
