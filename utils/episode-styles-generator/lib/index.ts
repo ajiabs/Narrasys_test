2c1d02135910508ab065874f8f26e164bd780a60
githop <tom@githop.com>
2017-07-14 16:09:32 -0600

    update(chef-ann template): fix spacing in cssGen.ts, add new template

--- utils/episode-styles-generator/lib/index.ts
+++ utils/episode-styles-generator/lib/index.ts
@@ -7,101 +7,119 @@ const pathToClientStyles = resolve('../../app/styles/');
 const pathToEpisodeScss = resolve('../../app/styles/episode.scss');
 
 const argv = require('yargs')
-    .option('nameSpace', {
-        alias: 'n',
-        describe: 'the template namespace',
-    })
-    .option('headerFont', {
-        alias: 'h',
-        describe: 'the font for headers'
-    })
-    .option('accentFont', {
-        alias: 'q',
-        describe: 'the font for headers'
-    })
-    .option('bodyFont', {
-        alias: 'b',
-        describe: 'the font for text'
-    })
-    .option('primaryColor', {
-        alias: 'p',
-        describe: 'the primary color',
-        type: 'string'
-    })
-    .option('secondaryColor', {
-        alias: 's',
-        describe: 'the secondary color',
-        type: 'string'
-    })
-    .option('accentColor', {
-        alias: 'a',
-        describe: 'the accent color',
-        type: 'string'
-    })
-    .option('linkColor', {
-        alias: 'm',
-        describe: 'A color that applies to links and timestamps',
-        type: 'string'
-    })
-	.option('highlightColor', {
-		alias: 'x',
-		describe: 'The color to use for highlights',
-		type: 'string'
-	})
-    .option('brandLogo', {
-        alias: 'l',
-        describe: 'the brand logo'
-    })
-    .option('bgMain', {
-        alias: 'v',
-        describe: 'the main col static background image'
-    })
-    .option('bgAlt', {
-        alias: 't',
-        describe: 'the alt col static background image'
-    })
-    .option('bgMondrian', {
-        alias: 'z',
-        describe: 'the mondrian alt col static background image'
-    })
-    .option('bgWindow', {
-        alias: 'w',
-        describe: 'The episode background image'
-    })
-    .argv;
+  .option('nameSpace', {
+    alias: 'n',
+    describe: 'the template namespace',
+  })
+  .option('headerFont', {
+    alias: 'h',
+    describe: 'the font for headers'
+  })
+  .option('accentFont', {
+    alias: 'q',
+    describe: 'the font for headers'
+  })
+  .option('bodyFont', {
+    alias: 'b',
+    describe: 'the font for text'
+  })
+  .option('primaryColor', {
+    alias: 'p',
+    describe: 'the primary color',
+    type: 'string'
+  })
+  .option('secondaryColor', {
+    alias: 's',
+    describe: 'the secondary color',
+    type: 'string'
+  })
+  .option('accentColor', {
+    alias: 'a',
+    describe: 'the accent color',
+    type: 'string'
+  })
+  .option('linkColor', {
+    alias: 'm',
+    describe: 'A color that applies to links and timestamps',
+    type: 'string'
+  })
+  .option('highlightColor', {
+    alias: 'x',
+    describe: 'The color to use for highlights',
+    type: 'string'
+  })
+  .option('brandLogo', {
+    alias: 'l',
+    describe: 'the brand logo'
+  })
+  .option('bgMain', {
+    alias: 'v',
+    describe: 'the main col static background image'
+  })
+  .option('bgAlt', {
+    alias: 't',
+    describe: 'the alt col static background image'
+  })
+  .option('bgMondrian', {
+    alias: 'z',
+    describe: 'the mondrian alt col static background image'
+  })
+  .option('bgWindow', {
+    alias: 'w',
+    describe: 'The episode background image'
+  })
+  .argv;
 
 const errhandler = (err) => {
-    if (err) console.error(err);
+  if (err) console.error(err);
 };
-const {nameSpace, accentColor, primaryColor, secondaryColor, linkColor, highlightColor, headerFont, bodyFont, accentFont, bgMain, bgAlt, bgMondrian, bgWindow, brandLogo} = argv;
+const {
+  nameSpace,
+  accentColor,
+  primaryColor,
+  secondaryColor,
+  linkColor,
+  highlightColor,
+  headerFont,
+  bodyFont,
+  accentFont,
+  bgMain,
+  bgAlt,
+  bgMondrian,
+  bgWindow,
+  brandLogo
+} = argv;
 
 const cssTemplate = genCss(argv);
 const htmlTemplate = genHtml(nameSpace, brandLogo);
 
-interface templateData {
-    path:string;
-    data:string;
+interface ITemplateData {
+  path: string;
+  data: string;
 }
 
-const data: templateData[] = [{path: `${pathToClientStyles}/_${nameSpace}.scss`, data: cssTemplate}, {path: `out/${nameSpace}.html`, data: htmlTemplate}];
+const data: ITemplateData[] = [{
+  path: `${pathToClientStyles}/_${nameSpace}.scss`,
+  data: cssTemplate
+}, {path: `out/${nameSpace}.html`, data: htmlTemplate}];
 
 const appendImport = () => {
-    fs.readFile(pathToEpisodeScss, 'utf8', (err, episodeScss) => {
-        if (err) throw err;
-        const match = `@import '${nameSpace}'`;
-        const re = new RegExp(match, 'g');
+  fs.readFile(pathToEpisodeScss, 'utf8', (err, episodeScss) => {
+    if (err) throw err;
+    const match = `@import '${nameSpace}'`;
+    const re = new RegExp(match, 'g');
 
-        if (!re.test(episodeScss)) {
-            episodeScss += `\n${match};`;
-            fs.writeFile(pathToEpisodeScss, episodeScss, errhandler);
-        } else {
-           console.log('not appending', nameSpace);
-        }
-    });
+    if (!re.test(episodeScss)) {
+      episodeScss += `\n${match};`;
+      fs.writeFile(pathToEpisodeScss, episodeScss, errhandler);
+    } else {
+      console.log('not appending', nameSpace);
+    }
+  });
 };
 
-data.forEach((blob:templateData) => {
-    fs.writeFile(blob.path, blob.data, errhandler);
+data.forEach((blob: ITemplateData) => {
+  fs.writeFile(blob.path, blob.data, errhandler);
 });
 
 appendImport();
