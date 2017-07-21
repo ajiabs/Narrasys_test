c9e55283190b1ceb85af4195c505177d7bed70a2
githop <tom@githop.com>
2017-07-21 14:46:54 -0600

    chore(magnet-perf): use scale to handle resizing video.

--- app/scripts/directives/ittMagnetized.ts
+++ app/scripts/directives/ittMagnetized.ts
@@ -1,52 +1,46 @@
 'use strict';
 
 // TODO: remove dependence on jQuery? (lots of it here)
-ittMagnetized.$inject = ['$rootScope', '$timeout', 'appState'];
-export default function ittMagnetized($rootScope, $timeout, appState) {
+ittMagnetized.$inject = ['$rootScope', '$timeout'];
+export default function ittMagnetized($rootScope, $timeout) {
   return {
     restrict: 'A',
     replace: true,
     scope: true,
     link: function (scope, element) {
-      const aspectRatio = 16 / 9;
       const WIDTH = 854;
       const HEIGHT = 480;
+      const TRANSITION_DURATION = '0.25s';
       element.css('top', 0);
       element.css('left', 0);
       element.css('position', 'fixed');
       element.css('will-change', 'transform');
-      // element.css('transform-origin', 'top left');
+      element.css('transform-origin', 'top left');
       element.width(WIDTH);
       element.height(HEIGHT);
 
       $rootScope.$on('magnet.changeMagnet', (evt, magnet) => jumpToMagnet(magnet, false));
       $rootScope.$on('magnet.jumpToMagnet', (evt, magnet) => jumpToMagnet(magnet, true));
 
-      function jumpToMagnet (magnet, jump: boolean) {
+      function jumpToMagnet (magnet, animateTransition: boolean) {
         if (magnet == null) {
           return;
         }
         $timeout(() => {
           const {top, left, width} = magnet[0].getBoundingClientRect();
 
-          if (jump === true) {
-            element.css('transition-duration', '0.25s');
+          if (animateTransition === true) {
+            element.css('transition-duration', TRANSITION_DURATION);
           }
 
-          // const scaleX = width / WIDTH;
-          // const scaleY = height / HEIGHT;
-          // console.log('magnet w', width, 'el width', WIDTH);
-          // console.log('magnet H', height, 'el H', HEIGHT);
-          // console.log('aspect W', width / aspectRatio);
-          // console.log('scales', scaleX, scaleY);
-          // console.log('bcr', left, top);
+          const scaleX = width / WIDTH;
 
-          element.css('transform', `translate(${left}px, ${top}px)`);
-          // element.css('transform', `scale(${scaleX}) translate(${left}px, ${top}px)`);
+          element.css('transform', `translate(${left}px, ${top}px) scale(${scaleX})`);
 
-          element.width(Math.ceil(width));
-          element.height(Math.ceil(width / aspectRatio));
-        }, 20);
+          // element.width(Math.ceil(width));
+          // element.height(Math.ceil(width / aspectRatio));
+
+        }, 100); //100ms timeout to allow the appropriate height value to be set when coming out of search mode
 
       }
 
