5466c07176efe48885d70fcfb2faf66dd77a9233
githop <tom@githop.com>
2017-08-29 09:46:05 -0600

    chore(code): remove old comment.

--- app/scripts/directives/ittScene.ts
+++ app/scripts/directives/ittScene.ts
@@ -12,8 +12,6 @@ export default function ittScene($timeout, appState) {
     template: '<span ng-include="scene.templateUrl"></span>',
     controller: 'SceneController',
     link: function (scope, element) {
-      // console.log('ittScene', scope, element, attrs);
-
       // force discover and watch modes to not start out scrolled halfway out of view (STORY-161)
       if (appState.viewMode !== 'review') {
         window.scrollTo(0, 0);
@@ -27,41 +25,6 @@ export default function ittScene($timeout, appState) {
       }
       twiddleScene();
 
-      //BEGIN temp code for debugging purposes
-      // setCurrentSeneNameOnAppState();
-      // function _getSceneName(sceneTemplate) {
-      // 	var _scenes = {
-      // 		centered: 'templates/scene/centered.html',
-      // 		centeredPro: 'templates/scene/centeredPro.html',
-      // 		'1col': 'templates/scene/1col.html',
-      // 		'2colL': 'templates/scene/2colL.html',
-      // 		'2colR': 'templates/scene/2colR.html',
-      // 		mirroredTwoCol: 'templates/scene/mirrored-twocol.html',
-      // 		cornerV: 'templates/scene/cornerV.html',
-      // 		centerVV: 'templates/scene/centerVV.html',
-      // 		centerVVMondrian: 'templates/scene/centerVV-Mondrian.html',
-      // 		cornerH: 'templates/scene/cornerH.html',
-      // 		pip: 'templates/scene/pip.html'
-      // 	};
-      //
-      // 	return Object.keys(_scenes).filter(function (key) {
-      // 		return _scenes[key] === sceneTemplate;
-      // 	})[0];
-      // }
-      //
-      // function setCurrentSeneNameOnAppState() {
-      // 	if (scope.scene) {
-      // 		appState.currSceneName = scope.scene.templateUrl;
-      // 		// appState.sceneLayouts = scope.scene.layouts;
-      // 	}
-      // }
-      //END temp code for debugging purposes
-
-      // if (scope.scene._id.match(/internal/)) {
-      // landing and ending screens get inter-episode nav:
-      // scope.containers = modelSvc.containers;
-      // scope.crossEpisodePath = appState.crossEpisodePath;
-      // }
 
       function isIframedIOS(): boolean {
         return (appState.isFramed && appState.iOSVersion && appState.iOSVersion[0] && appState.iOSVersion[0] > 0);
