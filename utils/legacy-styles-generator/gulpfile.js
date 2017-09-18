54949d2d2a87c151a76b421cc2e1af17fbd02b11
githop <tom@githop.com>
2017-09-18 13:51:04 -0600

    updates(episode-templates): generate legacy css with color / typo

--- utils/legacy-styles-generator/gulpfile.js
+++ utils/legacy-styles-generator/gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
 const argv = require('yargs').argv;
 
 const cssFileName = argv.css;
@@ -11,22 +12,29 @@ const replaceCssClass = argv.replace;
 const stylesDir = resolve('../../app/styles');
 const EPISODE_TEMPLATE_CSS_CLASS = 'episode-template';
 
-if (cssFileName == null || replaceCssClass == null) {
-  throw '-css and --replace flags are required';
-}
-
 /*
   example: ./node_modules/.bin/gulp legacy-css --replace episode-purdue --css episode-purdue.css
  */
 gulp.task('legacy-css', () => {
-  gulp.src(stylesDir + `/${cssFileName}`)
+
+  if (cssFileName == null || replaceCssClass == null) {
+    throw '-css and --replace flags are required';
+    return;
+  }
+
+  gulp.src(`./css/${cssFileName}`)
     .pipe(replace(replaceCssClass, EPISODE_TEMPLATE_CSS_CLASS))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(stylesDir));
+    .pipe(gulp.dest('./css'));
 });
 
+gulp.task('legacy-sass', () => {
+  return gulp.src('./legacy-styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
 
---
+++ utils/legacy-styles-generator/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+./node_modules/.bin/gulp legacy-css --replace episode-columbia --css episode-columbia.css
+./node_modules/.bin/gulp legacy-css --replace episode-columbiabusiness --css episode-columbiabusiness.css
+./node_modules/.bin/gulp legacy-css --replace episode-gw --css episode-gw.css
+./node_modules/.bin/gulp legacy-css --replace episode-gw --css episode-gw.css
+./node_modules/.bin/gulp legacy-css --replace episode-gwsb --css episode-gwsb.css
+./node_modules/.bin/gulp legacy-css --replace episode-kellogg --css episode-kellogg.css
+./node_modules/.bin/gulp legacy-css --replace episode-middulebury --css episode-middlebury.css
+./node_modules/.bin/gulp legacy-css --replace episode-prolotherapy --css episode-prolotherapy.css
+./node_modules/.bin/gulp legacy-css --replace episode-purdue --css episode-purdue.css
+./node_modules/.bin/gulp legacy-css --replace episode-regis --css episode-regis.css
+./node_modules/.bin/gulp legacy-css --replace episode-wiley --css episode-wiley.css
+./node_modules/.bin/gulp legacy-css --replace episode-wsbctc --css episode-wsbctc.css
