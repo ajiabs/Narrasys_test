10f3e74742b31f78cab464f6a0d7a34aec475cb9
githop <tom@githop.com>
2017-09-19 12:49:56 -0600

    updates(legacy-styles): replace url paths, simplify gulp pipeline

--- utils/legacy-styles-generator/gulpfile.js
+++ utils/legacy-styles-generator/gulpfile.js
@@ -1,40 +1,36 @@
 
-const { resolve } = require('path');
+const { basename } = require('path');
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
-const argv = require('yargs').argv;
+const tap = require('gulp-tap');
 
-const cssFileName = argv.css;
-const replaceCssClass = argv.replace;
-const stylesDir = resolve('../../app/styles');
 const EPISODE_TEMPLATE_CSS_CLASS = 'episode-template';
-
+const EPISODE_IMAGE_PATH = 'https://s3.amazonaws.com/narrasys.static.uploads/production/customer/';
+const BG_URL_REGEX = /url\((.*?)\)/g;
 /*
-  example: ./node_modules/.bin/gulp legacy-css --replace episode-purdue --css episode-purdue.css
- */
-gulp.task('legacy-css', () => {
-
-  if (cssFileName == null || replaceCssClass == null) {
-    throw '-css and --replace flags are required';
-    return;
-  }
-
-  gulp.src(`./css/${cssFileName}`)
-    .pipe(replace(replaceCssClass, EPISODE_TEMPLATE_CSS_CLASS))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
-});
-
+  for this to work there must be a convention where the file name is the same
+  as the customer css class namespace in the css file.
+  eg. file: episode-columbia.scss  css class: .episode-columbia {}
+*/
 gulp.task('legacy-sass', () => {
   return gulp.src('./legacy-styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(tap((file) => {
+      const fileName = basename(file.path);
+      const replaceCssClass = fileName.split('.')[0];
+      return gulp.src(`./legacy-styles/${fileName}`)
+        .pipe(replace(replaceCssClass, EPISODE_TEMPLATE_CSS_CLASS))
+        .pipe(replace(BG_URL_REGEX, (match) => {
+          const fileName = match.slice(match.lastIndexOf('/') + 1, -2);
+          return `url("${EPISODE_IMAGE_PATH + fileName}")`
+        }))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions'],
+          cascade: false
+        }))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+    }))
 });
 
