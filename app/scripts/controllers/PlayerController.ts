fa413c228b1bb9321d651db7e3dddad6fd957db3
githop <tom@githop.com>
2017-09-26 15:57:31 -0600

    chore(code): remove unused constant import

--- app/scripts/controllers/PlayerController.ts
+++ app/scripts/controllers/PlayerController.ts
@@ -1,7 +1,7 @@
 //TODO Some of this could be split into separate controllers (though that may not confer any advantage other than keeping this file small...)
 
 import {IModelSvc, IDataSvc} from '../interfaces';
-import { JUMP_TO_MAGNET, UPDATE_MAGNET } from '../constants';
+import { UPDATE_MAGNET } from '../constants';
 PlayerController.$inject = ['$scope', '$location', '$rootScope', '$routeParams', '$timeout', '$interval', 'config', 'appState', 'dataSvc', 'modelSvc', 'timelineSvc', 'analyticsSvc', 'authSvc', 'selectService', 'playbackService'];
 
 export default function PlayerController($scope, $location, $rootScope, $routeParams, $timeout, $interval, config, appState, dataSvc: IDataSvc, modelSvc: IModelSvc, timelineSvc, analyticsSvc, authSvc, selectService, playbackService) {
