f528857cd5934cba819b58e46c3c13235966e37b
githop <tom@githop.com>
2018-01-25 14:46:44 -0700

    updates(flags): fix invert for annotations

--- app/player/input-fields/directives/ittDynamicModel.ts
+++ app/player/input-fields/directives/ittDynamicModel.ts
@@ -3,12 +3,11 @@
  * Created by githop on 7/1/16.
  */
 
-
 export class DynamicModel implements ng.IDirective {
   restrict: string = 'A';
   terminal = true;
   priority = 100000;
-  static Name = 'npDynamicModel';
+  static Name = 'npDynamicModel'; //tslint:disable-line
   static $inject = ['$compile', '$parse'];
 
   constructor(private $compile: ng.ICompileService, private $parse: ng.IParseService) {
@@ -27,22 +26,5 @@ export class DynamicModel implements ng.IDirective {
     elm.attr('ng-model', name);
     this.$compile(elm)(scope);
   }
-}
-
-ittDynamicModel.$inject = ['$compile', '$parse'];
-
-//http://stackoverflow.com/a/32096328
-export default function ittDynamicModel($compile, $parse) {
-  return {
-    restrict: 'A',
-    terminal: true,
-    priority: 100000,
-    link: function (scope, elem) {
-      var name = $parse(elem.attr('itt-dynamic-model'))(scope);
-      elem.removeAttr('itt-dynamic-model');
-      elem.attr('ng-model', name);
-      $compile(elem)(scope);
-    }
-  };
 }
 
